94a59a8f66774267eca8b63724b69262a1a3b310
Sphyix <masifede2@gmail.com>
2021-07-30 22:47:54 +0200

    Mixins for IC2 Hazmat (#26)
    * added mixins for ic2 hazmat
    
    * comment to explain Overwrite reason

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/fixIc2Hazmat/MixinIc2Hazmat.java
@@ -0,0 +1,20 @@
+package com.mitchej123.hodgepodge.mixins.fixIc2Hazmat;
+
+import ic2.core.item.armor.ItemArmorHazmat;
+import net.minecraft.entity.EntityLivingBase;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import gregtech.api.util.GT_Utility;
+
+@Mixin(value = ItemArmorHazmat.class, remap = false)
+
+public class MixinIc2Hazmat {
+    @Overwrite()
+    //IC2 logic superseeded by GT check
+    public static boolean hasCompleteHazmat(EntityLivingBase entity) {
+        if(GT_Utility.isWearingFullRadioHazmat(entity)) {
+            return true;
+        }
+        return false;
+    }
+}
