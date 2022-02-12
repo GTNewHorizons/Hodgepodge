73321831625fdc718ae721c9d0400974654dbb8c
Jason Mitchell <mitchej@gmail.com>
2022-02-11 20:48:10 -0800

    Change limit to a config option

--- src/main/java/com/mitchej123/hodgepodge/mixins/minecraft/MixinEffectRenderer.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/minecraft/MixinEffectRenderer.java
@@ -1,6 +1,7 @@
 package com.mitchej123.hodgepodge.mixins.minecraft;
 
 import com.mitchej123.hodgepodge.Hodgepodge;
+import com.mitchej123.hodgepodge.core.HodgepodgeMixinPlugin;
 import net.minecraft.client.particle.EffectRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
@@ -10,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 public class MixinEffectRenderer {
     @ModifyConstant(method= "addEffect(Lnet/minecraft/client/particle/EntityFX;)V", constant = @Constant(intValue = 4000, ordinal=0), require = 1)
     private int getParticleLimit(int constant) {
-        return 16384;
+        return Hodgepodge.config.particleLimit;
     }
 }
