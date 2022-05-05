7c429fc00fa78971cdd217f1735f299f8e2c2c22
Glease <4586901+Glease@users.noreply.github.com>
2022-05-05 16:36:34 +0800

    Improve information displayed

--- src/main/java/com/mitchej123/hodgepodge/mixins/minecraft/MixinS02PacketChat.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/minecraft/MixinS02PacketChat.java
@@ -4,6 +4,7 @@ import com.mitchej123.hodgepodge.Hodgepodge;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.play.server.S02PacketChat;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent.Serializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -25,10 +26,10 @@ public abstract class MixinS02PacketChat {
             if (Hodgepodge.config.logHugeChat) {
                 String incidentId = "" + System.currentTimeMillis() + ThreadLocalRandom.current().nextInt(1000);
                 LOGGER.info("HUGE chat message caught. Incident ID {}. Serialized message {}.", incidentId, s);
-                bytes = Serializer.func_150696_a(new ChatComponentText("Someone tried to sent you a huge chat message that would kick you. Ask your server admin to see details. Incident ID " + incidentId)).getBytes(StandardCharsets.UTF_8);
+                bytes = Serializer.func_150696_a(new ChatComponentText(EnumChatFormatting.RED + "Someone tried to sent you a huge chat message that would kick you. Ask your server admin to see details. Please provide these info while reporting: Incident ID " + EnumChatFormatting.UNDERLINE + incidentId)).getBytes(StandardCharsets.UTF_8);
             } else {
                 LOGGER.info("HUGE chat message caught. Details are not logged here as requested in config.");
-                bytes = Serializer.func_150696_a(new ChatComponentText("Someone tried to sent you a huge chat message that would kick you. Ask your server admin to turn on logHugeChat in HodgePodge if this keeps happening.")).getBytes(StandardCharsets.UTF_8);
+                bytes = Serializer.func_150696_a(new ChatComponentText(EnumChatFormatting.RED + "Someone tried to sent you a huge chat message that would kick you. Ask your server admin to turn on logHugeChat in HodgePodge if this keeps happening.")).getBytes(StandardCharsets.UTF_8);
             }
         }
         instance.writeVarIntToBuffer(bytes.length);
