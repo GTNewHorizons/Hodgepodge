a486f71bc6680a7807577616877c48a11b2a0241
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-05-13 09:16:29 -0400

    Fix ProjectE being incompatible with smelting map replacement

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/projecte/MixinObjHandler.java
@@ -0,0 +1,19 @@
+package com.mitchej123.hodgepodge.mixins.projecte;
+
+import moze_intel.projecte.gameObjs.ObjHandler;
+import net.minecraft.item.crafting.FurnaceRecipes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Mixin(ObjHandler.class)
+public class MixinObjHandler {
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @Redirect(method = "registerPhiloStoneSmelting", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/crafting/FurnaceRecipes;func_77599_b()Ljava/util/Map;"), remap = false)
+    private static Map getFakeRecipeMap(FurnaceRecipes instance) {
+        return new HashMap(instance.getSmeltingList());
+    }
+}
