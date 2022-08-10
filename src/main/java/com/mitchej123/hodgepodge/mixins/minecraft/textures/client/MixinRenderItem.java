07d2915ba5493828050ad95452d9f75324185cc9
Vladislav Laetansky <vlaetansky@gmail.com>
2022-08-10 15:06:21 +0300

    Fix class cast exception

--- src/main/java/com/mitchej123/hodgepodge/mixins/minecraft/textures/client/MixinRenderItem.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/minecraft/textures/client/MixinRenderItem.java
@@ -2,6 +2,7 @@ package com.mitchej123.hodgepodge.mixins.minecraft.textures.client;
 
 import com.mitchej123.hodgepodge.core.textures.IPatchedTextureAtlasSprite;
 import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.util.IIcon;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,6 +18,8 @@ public class MixinRenderItem {
      */
     @Inject(method = "renderIcon", at = @At("HEAD"))
     public void beforeRenderIcon(int p_94149_1_, int p_94149_2_, IIcon icon, int p_94149_4_, int p_94149_5_, CallbackInfo ci) {
-        ((IPatchedTextureAtlasSprite) icon).markNeedsAnimationUpdate();
+        if (icon instanceof TextureAtlasSprite) {
+            ((IPatchedTextureAtlasSprite) icon).markNeedsAnimationUpdate();
+        }
     }
 }
