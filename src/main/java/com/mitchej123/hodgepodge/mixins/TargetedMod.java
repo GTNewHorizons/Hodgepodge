6e3f858b4e5fe1d5649dfabd079a0b0b9e46837b
Raven Szewczyk <git@eigenraven.me>
2022-09-26 18:29:05 +0100

    Make Journeymap an optional mod (#128)

--- src/main/java/com/mitchej123/hodgepodge/mixins/TargetedMod.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/TargetedMod.java
@@ -19,7 +19,7 @@ public enum TargetedMod {
     TRAVELLERSGEAR("TravellersGear", "TravellersGear"),
     // Temporary solution to force load it early
     GTNHLIB("GTNHLib", "gtnhlib"),
-    JOURNEYMAP("journeymap", "journeymap");
+    JOURNEYMAP("journeymap", "journeymap", true);
     // OPTIFINE("OptiFine", "OptiFine");
 
     public final String modName;
