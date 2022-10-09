a719b8b080ca129012238ca3e39a0ae028fa4d6d
Jason Mitchell <mitchej@gmail.com>
2022-10-09 14:43:08 -0700

    Merge ModMixins back into Hodgepodge (#134)
    Merge ModMixins back into Hodgepodge
    
    * Disable Spotless on LoadingConfig; it's a nightmare to read otherwise!
    * Add a PollutionHelper SHIM
    * Add missing Configs
    * Helpful Exception if missing applyIf()
    * Fix multiple excluded mods
    * Move a few mixins to early, add GC coremod detection (so we can properly identify the GC command fix requirement)

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinTileEntityFurnacePollution.java
@@ -0,0 +1,32 @@
+package com.mitchej123.hodgepodge.mixins.early.minecraft;
+
+import com.mitchej123.hodgepodge.Common;
+import com.mitchej123.hodgepodge.util.PollutionHelper;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityFurnace;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.libraries.org.objectweb.asm.Opcodes;
+
+/*
+ * Merged from ModMixins under the MIT License
+ *    Copyright bartimaeusnek & GTNewHorizons
+ */
+@Mixin(TileEntityFurnace.class)
+public abstract class MixinTileEntityFurnacePollution extends TileEntity {
+    @Inject(
+            method = "updateEntity",
+            at =
+                    @At(
+                            value = "FIELD",
+                            target = "net/minecraft/tileentity/TileEntityFurnace.furnaceBurnTime:I",
+                            opcode = Opcodes.PUTFIELD))
+    void addPollution(CallbackInfo ci) {
+        if (!this.worldObj.isRemote && (this.worldObj.getTotalWorldTime() % 20) == 0)
+            PollutionHelper.addPollution(
+                    this.worldObj.getChunkFromBlockCoords(this.xCoord, this.zCoord),
+                    Common.config.furnacePollutionAmount);
+    }
+}
---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/galacticraftcore/MixinGalacticraftRocketPollution.java
@@ -0,0 +1,41 @@
+package com.mitchej123.hodgepodge.mixins.late.galacticraftcore;
+
+import com.mitchej123.hodgepodge.Common;
+import com.mitchej123.hodgepodge.util.PollutionHelper;
+import micdoodle8.mods.galacticraft.api.entity.IRocketType;
+import micdoodle8.mods.galacticraft.api.prefab.entity.EntityAutoRocket;
+import micdoodle8.mods.galacticraft.api.prefab.entity.EntityTieredRocket;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/*
+ * Merged from ModMixins under the MIT License
+ *    Copyright bartimaeusnek & GTNewHorizons
+ */
+@Mixin(EntityTieredRocket.class)
+public abstract class MixinGalacticraftRocketPollution extends EntityAutoRocket implements IRocketType {
+
+    public MixinGalacticraftRocketPollution(World world) {
+        super(world);
+    }
+
+    @Inject(method = "onUpdate", at = @At("HEAD"))
+    private void addRocketPollution(CallbackInfo ci) {
+        if (this.worldObj.isRemote
+                || !(launchPhase == EnumLaunchPhase.LAUNCHED.ordinal()
+                        || launchPhase == EnumLaunchPhase.IGNITED.ordinal())) return;
+        int pollutionAmount = Common.config.rocketPollutionAmount;
+
+        // Note: Linear instead of growing by powers of 2
+        if (launchPhase == EnumLaunchPhase.LAUNCHED.ordinal())
+            pollutionAmount = (pollutionAmount * this.getRocketTier());
+        else if (launchPhase == EnumLaunchPhase.IGNITED.ordinal())
+            pollutionAmount = (pollutionAmount * (this.getRocketTier())) / 100;
+        else pollutionAmount = 0;
+
+        PollutionHelper.addPollution(worldObj.getChunkFromBlockCoords((int) posX, (int) posZ), pollutionAmount);
+    }
+}
