f3bd4c59ca9e7a32a42dd5b9ea8300840f7e2a0b
Jason Mitchell <mitchej@gmail.com>
2022-10-08 17:46:25 -0700

    Faster tileentity removal for World.updateEntities Don't apply if fastcraft is loaded Not compatible with Thermos.   Should probably add that as an excluded mod at this point... Bukkit as a TargetedMod * Bump GTNH Mixins -> 2.0.1

--- src/main/java/com/mitchej123/hodgepodge/mixins/Mixins.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/Mixins.java
@@ -228,6 +228,13 @@ public enum Mixins {
             .addMixinClasses("minecraft.MixinDimensionManager")
             .setApplyIf(() -> Common.config.dimensionManagerDebug)
             .addTargetedMod(TargetedMod.VANILLA)),
+    OPTIMIZE_TILEENTITY_REMOVAL(new Builder("Optimize TileEntity Removal")
+            .setPhase(Phase.EARLY)
+            .addMixinClasses("minecraft.MixinWorldUpdateEntities")
+            .setApplyIf(() -> Common.config.optimizeTileentityRemoval)
+            .addTargetedMod(TargetedMod.VANILLA)
+            .addExcludedMod(TargetedMod.FASTCRAFT)
+            .addExcludedMod(TargetedMod.BUKKIT)),
     FIX_POTION_ITERATING(new Builder("Fix Potion Iterating")
             .setPhase(Phase.EARLY)
             .addMixinClasses("minecraft.MixinEntityLivingBase_FixPotionException")
