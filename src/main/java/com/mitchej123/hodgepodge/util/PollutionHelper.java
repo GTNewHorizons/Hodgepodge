26c40f79e7e931fad2d26bbb3a97243559a0c8ec
makamys <makamys@outlook.com>
2022-11-29 17:27:21 +0100

    Check for GT5's presence before using GT_Pollution
    Fixes crash when furnacesPollute is enabled without GT5 being present.

--- src/main/java/com/mitchej123/hodgepodge/util/PollutionHelper.java
+++ src/main/java/com/mitchej123/hodgepodge/util/PollutionHelper.java
@@ -1,5 +1,6 @@
 package com.mitchej123.hodgepodge.util;
 
+import com.mitchej123.hodgepodge.Compat;
 import gregtech.common.GT_Pollution;
 import net.minecraft.world.chunk.Chunk;
 
@@ -8,6 +9,8 @@ public class PollutionHelper {
      * GT might not loaded when the pollution mixins run, so use this shim
      */
     public static void addPollution(Chunk ch, int aPollution) {
-        GT_Pollution.addPollution(ch, aPollution);
+        if (Compat.isGT5Present()) {
+            GT_Pollution.addPollution(ch, aPollution);
+        }
     }
 }
