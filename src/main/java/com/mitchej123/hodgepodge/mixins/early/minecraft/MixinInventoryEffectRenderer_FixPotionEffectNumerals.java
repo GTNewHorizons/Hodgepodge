3435269316366016da9c703c29e0349507edf2fb
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-01-08 00:20:48 +0100

    use @ModifyReturnValue instead of @Redirect

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinInventoryEffectRenderer_FixPotionEffectNumerals.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinInventoryEffectRenderer_FixPotionEffectNumerals.java
@@ -1,9 +1,9 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
+import com.gtnewhorizon.mixinextras.injector.ModifyExpressionValue;
 import com.mitchej123.hodgepodge.Common;
 import com.mitchej123.hodgepodge.util.RomanNumerals;
 import net.minecraft.client.renderer.InventoryEffectRenderer;
-import net.minecraft.potion.PotionEffect;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -13,11 +13,11 @@ public class MixinInventoryEffectRenderer_FixPotionEffectNumerals {
 
     private int hodgepodge$potionAmplifierLevel;
 
-    @Redirect(
+    @ModifyExpressionValue(
             method = "func_147044_g",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/potion/PotionEffect;getAmplifier()I", ordinal = 0))
-    public int hodgepodge$skipOriginalCode(PotionEffect potionEffect) {
-        this.hodgepodge$potionAmplifierLevel = potionEffect.getAmplifier();
+    private int hodgepodge$skipOriginalCode(int amplifier) {
+        this.hodgepodge$potionAmplifierLevel = amplifier;
         return 1;
     }
 
@@ -29,7 +29,7 @@ public class MixinInventoryEffectRenderer_FixPotionEffectNumerals {
                             target =
                                     "Lnet/minecraft/client/resources/I18n;format(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;",
                             ordinal = 1))
-    public String hodgepodge$addRomanNumeral(String string, Object... objects) {
+    private String hodgepodge$addRomanNumeral(String string, Object... objects) {
         if (this.hodgepodge$potionAmplifierLevel > 0) {
             if (Common.config.arabicNumbersForEnchantsPotions) {
                 return String.valueOf(this.hodgepodge$potionAmplifierLevel + 1);
--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinItemPotion_FixRomanNumerals.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinItemPotion_FixRomanNumerals.java
@@ -1,9 +1,9 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
+import com.gtnewhorizon.mixinextras.injector.ModifyExpressionValue;
 import com.mitchej123.hodgepodge.Common;
 import com.mitchej123.hodgepodge.util.RomanNumerals;
 import net.minecraft.item.ItemPotion;
-import net.minecraft.potion.PotionEffect;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -13,12 +13,12 @@ public class MixinItemPotion_FixRomanNumerals {
 
     private int hodgepodge$potionAmplifierLevel;
 
-    @Redirect(
+    @ModifyExpressionValue(
             method = "addInformation",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/potion/PotionEffect;getAmplifier()I", ordinal = 1))
-    public int hodgepodge$getAmplifierLevel(PotionEffect potionEffect) {
-        this.hodgepodge$potionAmplifierLevel = potionEffect.getAmplifier();
-        return potionEffect.getAmplifier();
+    private int hodgepodge$getAmplifierLevel(int amplifier) {
+        this.hodgepodge$potionAmplifierLevel = amplifier;
+        return amplifier;
     }
 
     @Redirect(
@@ -29,7 +29,7 @@ public class MixinItemPotion_FixRomanNumerals {
                             target =
                                     "Lnet/minecraft/util/StatCollector;translateToLocal(Ljava/lang/String;)Ljava/lang/String;",
                             ordinal = 1))
-    public String hodgepodge$addRomanNumeral(String string) {
+    private String hodgepodge$addRomanNumeral(String string) {
         if (Common.config.arabicNumbersForEnchantsPotions) {
             return String.valueOf(this.hodgepodge$potionAmplifierLevel + 1);
         } else {
