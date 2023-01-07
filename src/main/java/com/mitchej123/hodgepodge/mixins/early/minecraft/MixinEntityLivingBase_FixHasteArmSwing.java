3f0d84afbd610880710ef75412b10b708d3e0ad4
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-01-07 14:18:47 +0100

    use @ModifyReturnValue instead of @Inject

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinEntityLivingBase_FixHasteArmSwing.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinEntityLivingBase_FixHasteArmSwing.java
@@ -1,18 +1,15 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
+import com.gtnewhorizon.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.entity.EntityLivingBase;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityLivingBase.class)
 public class MixinEntityLivingBase_FixHasteArmSwing {
 
-    @Inject(method = "getArmSwingAnimationEnd", at = @At("RETURN"), cancellable = true)
-    public void hodgepodge$fixHasteArmSwing(CallbackInfoReturnable<Integer> cir) {
-        if (cir.getReturnValueI() < 2) {
-            cir.setReturnValue(2);
-        }
+    @ModifyReturnValue(method = "getArmSwingAnimationEnd", at = @At("RETURN"))
+    private int hodgepodge$fixHasteArmSwing(int original) {
+        return Math.max(original, 2);
     }
 }
