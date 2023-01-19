1c14b75e033ff95b62d7cc56574eeb112e2d7692
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-01-19 09:00:38 +0100

    disable version checking for journeymap and bibliocraft (#163)
    * yeet bibliocraft version checker
    
    * yeet journeymap version checker

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/bibliocraft/MixinVersionCheck.java
@@ -0,0 +1,37 @@
+package com.mitchej123.hodgepodge.mixins.late.bibliocraft;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import jds.bibliocraft.VersionCheck;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(value = VersionCheck.class, remap = false)
+public class MixinVersionCheck {
+
+    /**
+     * @author Alexdoru
+     * @reason yeet
+     */
+    @Overwrite
+    @SubscribeEvent
+    public void onWorldLoad(EntityJoinWorldEvent event) {
+        FMLCommonHandler.instance().bus().unregister(this);
+    }
+
+    /**
+     * @author Alexdoru
+     * @reason yeet
+     */
+    @Overwrite
+    public static void getNetVersion(EntityPlayer player) {}
+
+    /**
+     * @author Alexdoru
+     * @reason yeet
+     */
+    @Overwrite
+    public static void setUpdateMessage(EntityPlayer player) {}
+}
