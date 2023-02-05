66162737959987082836778735b87d6549859f80
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-02-05 21:44:05 +0100

    unbind cofh keybinds by default

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/cofhcore/MixinProxyClient.java
@@ -0,0 +1,18 @@
+package com.mitchej123.hodgepodge.mixins.early.cofhcore;
+
+import org.lwjgl.input.Keyboard;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+import cofh.core.ProxyClient;
+
+@Mixin(ProxyClient.class)
+public class MixinProxyClient {
+
+    @ModifyConstant(method = "<clinit>", constant = { @Constant(intValue = 46), @Constant(intValue = 47) })
+    private static int hodgepodge$UnbindKeybind(int original) {
+        return Keyboard.KEY_NONE;
+    }
+
+}
