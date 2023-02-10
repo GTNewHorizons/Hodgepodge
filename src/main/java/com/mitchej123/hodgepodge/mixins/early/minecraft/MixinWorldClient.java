dde7a7663edc76c8f72cac39038c1fe712be842b
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-02-10 22:24:27 +0100

    stop playing a sound when spawning a minecart in the world (#181)

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinWorldClient.java
@@ -0,0 +1,23 @@
+package com.mitchej123.hodgepodge.mixins.early.minecraft;
+
+import net.minecraft.client.audio.ISound;
+import net.minecraft.client.audio.SoundHandler;
+import net.minecraft.client.multiplayer.WorldClient;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(WorldClient.class)
+public class MixinWorldClient {
+
+    @Redirect(
+            method = "spawnEntityInWorld",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/audio/SoundHandler;playSound(Lnet/minecraft/client/audio/ISound;)V"))
+    private void hodgepodge$CancelSound(SoundHandler soundHandler, ISound isound) {
+        // do nothing
+    }
+
+}
