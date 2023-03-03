01acbb980e9a3d01e9d64c4cb8349af9590b3935
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-03-03 13:21:00 +0100

    use ModifyConstant instead

--- src/main/java/com/mitchej123/hodgepodge/mixins/late/xaeroworldmap/MixinGuiMap.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/xaeroworldmap/MixinGuiMap.java
@@ -1,17 +1,16 @@
 package com.mitchej123.hodgepodge.mixins.late.xaeroworldmap;
 
-import org.lwjgl.input.Mouse;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import xaero.map.gui.GuiMap;
 
 @Mixin(GuiMap.class)
 public class MixinGuiMap {
 
-    @ModifyVariable(method = "handleMouseInput", at = @At("STORE"), ordinal = 0)
+    @ModifyConstant(method = "handleMouseInput", constant = @Constant(intValue = 120, ordinal = 0))
     private int hodgepodge$fixScrolling(int original) {
-        return Mouse.getEventDWheel();
+        return 1;
     }
 }
