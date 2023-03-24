2919e6a5fc83a4dac287f25ba7103f0be576b02a
Glease <4586901+Glease@users.noreply.github.com>
2023-03-24 17:19:36 +0800

    fix rare different sprite animations out of sync (#202)
    Signed-off-by: Glease <4586901+Glease@users.noreply.github.com>

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/textures/client/MixinTextureAtlasSprite.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/textures/client/MixinTextureAtlasSprite.java
@@ -1,8 +1,12 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft.textures.client;
 
+import java.util.List;
+
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.data.AnimationMetadataSection;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
 import com.mitchej123.hodgepodge.textures.IPatchedTextureAtlasSprite;
@@ -12,6 +16,17 @@ public class MixinTextureAtlasSprite implements IPatchedTextureAtlasSprite {
 
     @Unique
     private boolean needsAnimationUpdate = false;
+
+    @Shadow
+    protected int tickCounter;
+    @Shadow
+    protected int frameCounter;
+
+    @Shadow
+    private AnimationMetadataSection animationMetadata;
+
+    @Shadow
+    protected List<?> framesTextureData;
 
     @Override
     public void markNeedsAnimationUpdate() {
@@ -26,5 +41,16 @@ public class MixinTextureAtlasSprite implements IPatchedTextureAtlasSprite {
     @Override
     public void unmarkNeedsAnimationUpdate() {
         needsAnimationUpdate = false;
+    }
+
+    @Override
+    public void updateAnimationsDryRun() {
+        tickCounter++;
+        if (tickCounter >= animationMetadata.getFrameTimeSingle(frameCounter)) {
+            int j = this.animationMetadata.getFrameCount() == 0 ? framesTextureData.size()
+                    : this.animationMetadata.getFrameCount();
+            this.frameCounter = (this.frameCounter + 1) % j;
+            this.tickCounter = 0;
+        }
     }
 }
