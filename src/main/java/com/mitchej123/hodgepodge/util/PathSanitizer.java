7ed07b8b0f2e2b43eb892ec264001dc3d9e99c5b
Raven Szewczyk <git@eigenraven.me>
2023-07-28 20:19:00 +0100

    Fix path handling of bibliocraft saved books to replace invalid characters with dashes

---
+++ src/main/java/com/mitchej123/hodgepodge/util/PathSanitizer.java
@@ -0,0 +1,26 @@
+package com.mitchej123.hodgepodge.util;
+
+import java.util.regex.Pattern;
+
+/**
+ * Utilities for sanitizing file paths generated with user input
+ */
+public final class PathSanitizer {
+
+    // This class just has some static utilities
+    private PathSanitizer() {}
+
+    // On Linux it's just /, macOS forbids / and :
+    // On Windows it's <>:"/\|?*
+    private static final Pattern ILLEGAL_FILE_NAME_CHARS = Pattern.compile("[/\\\\<>:\\\"|?*]");
+
+    /**
+     * Replaces all illegal characters (including directory separators) for a file name from input with dashes.
+     * 
+     * @param input The untrusted file name
+     * @return A name that can be trusted to be safe for file reading/creation
+     */
+    public static String sanitizeFileName(String input) {
+        return ILLEGAL_FILE_NAME_CHARS.matcher(input).replaceAll("-").replace('\0', '-');
+    }
+}
