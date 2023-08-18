1ca4c2e3be18c9624e38ec29c261a3563db0237b
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-08-18 03:56:33 +0200

    Should reduce Fps lags caused by MixinRenderGlobal
    https://cdn.discordapp.com/attachments/1022680913122951258/1141913141329014975/image.png

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinRenderGlobal.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinRenderGlobal.java
@@ -12,7 +12,6 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ChatComponentText;
 
-import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -46,7 +45,10 @@ public class MixinRenderGlobal {
                     shift = At.Shift.AFTER,
                     by = 1))
     public void hodgepodge$prepareTESR(EntityLivingBase p_147589_1_, ICamera p_147589_2_, float p_147589_3_,
-            CallbackInfo ci) {
+                                       CallbackInfo ci) {
+        ManagedEnum<RenderDebugMode> renderDebugMode = HodgepodgeClient.renderDebugMode;
+        if (renderDebugMode.is(RenderDebugMode.OFF)) return;
+
         RenderDebugHelper.recordGLStates();
     }
 
@@ -57,39 +59,30 @@ public class MixinRenderGlobal {
                     target = "Lnet/minecraft/client/renderer/tileentity/TileEntityRendererDispatcher;renderTileEntity(Lnet/minecraft/tileentity/TileEntity;F)V"))
     public void hodgepodge$postTESR(TileEntityRendererDispatcher instance, TileEntity j, float k) {
         ManagedEnum<RenderDebugMode> renderDebugMode = HodgepodgeClient.renderDebugMode;
-        if (!renderDebugMode.is(RenderDebugMode.OFF))
-            // this should be enough
-            GL11.glPushAttrib(
-                    GL11.GL_ENABLE_BIT | GL11.GL_DEPTH_BUFFER_BIT | GL11.GL_COLOR_BUFFER_BIT | GL11.GL_LIGHTING_BIT);
 
-        instance.renderTileEntity(j, k);
+        if (!renderDebugMode.is(RenderDebugMode.OFF)) {
+            instance.renderTileEntity(j, k);
 
-        if (renderDebugMode.is(RenderDebugMode.OFF)) {
-            knownIssues.clear();
-            return;
+            if (!knownIssues.contains(j) && !RenderDebugHelper.checkGLStates()) {
+                knownIssues.add(j);
+                Minecraft.getMinecraft().thePlayer.addChatMessage(
+                        new ChatComponentText(
+                                "TileEntity (" + j.getClass().getName()
+                                        + " at "
+                                        + j.xCoord
+                                        + ", "
+                                        + j.yCoord
+                                        + ", "
+                                        + j.zCoord
+                                        + ") is messing up render states!"));
+                RenderDebugHelper.log.error(
+                        "TileEntity {} at ({}, {}, {}) alter render state after TESR call: {}",
+                        j.getClass(),
+                        j.xCoord,
+                        j.yCoord,
+                        j.zCoord,
+                        RenderDebugHelper.compose());
+            }
         }
-
-        if (!knownIssues.contains(j) && !RenderDebugHelper.checkGLStates()) {
-            knownIssues.add(j);
-            Minecraft.getMinecraft().thePlayer.addChatMessage(
-                    new ChatComponentText(
-                            "TileEntity (" + j.getClass().getName()
-                                    + " at "
-                                    + j.xCoord
-                                    + ", "
-                                    + j.yCoord
-                                    + ", "
-                                    + j.zCoord
-                                    + ") is messing up render states!"));
-            RenderDebugHelper.log.error(
-                    "TileEntity {} at ({}, {}, {}) alter render state after TESR call: {}",
-                    j.getClass(),
-                    j.xCoord,
-                    j.yCoord,
-                    j.zCoord,
-                    RenderDebugHelper.compose());
-        }
-
-        GL11.glPopAttrib();
     }
 }
