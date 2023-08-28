4dc062b4a765107f1691ec52b4ed838db6112d54
kumquat-ir <66188216+kumquat-ir@users.noreply.github.com>
2023-08-28 12:44:37 -0400

    spotless

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinAbstractClientPlayer.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinAbstractClientPlayer.java
@@ -1,13 +1,17 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
 import net.minecraft.client.entity.AbstractClientPlayer;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(AbstractClientPlayer.class)
 public class MixinAbstractClientPlayer {
-    @ModifyConstant(method = "getDownloadImageSkin", constant = @Constant(stringValue = "http://skins.minecraft.net/MinecraftSkins/%s.png"))
+
+    @ModifyConstant(
+            method = "getDownloadImageSkin",
+            constant = @Constant(stringValue = "http://skins.minecraft.net/MinecraftSkins/%s.png"))
     private static String hodgepodge$redirectSkinUrl(String url) {
         return "https://visage.surgeplay.com/skin/%s.png";
     }
