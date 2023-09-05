ce0da4bce1d3e124b52982cc5f28a83eac06392b
miozune <miozune@gmail.com>
2023-09-05 14:27:03 +0900

    Fix another lag caused by IC2 spamming detectAndSendChanges

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/ic2/MixinElectricItemManager.java
@@ -0,0 +1,19 @@
+package com.mitchej123.hodgepodge.mixins.late.ic2;
+
+import net.minecraft.inventory.Container;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import ic2.core.item.ElectricItemManager;
+
+@Mixin(ElectricItemManager.class)
+public class MixinElectricItemManager {
+
+    @Redirect(
+            method = "chargeFromArmor",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/inventory/Container;detectAndSendChanges()V"),
+            remap = false)
+    public void hodgepodge$cancelDetectAndSendChanges(Container instance) {}
+}
