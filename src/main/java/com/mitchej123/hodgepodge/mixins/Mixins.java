28706469f020537b81178ef409f9c6b39552cbe8
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-09-17 17:33:45 +0200

    Fix wrapped chat lines missing colors (#246)
    Fix wrapped chat lines missing colors

--- src/main/java/com/mitchej123/hodgepodge/mixins/Mixins.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/Mixins.java
@@ -256,6 +256,10 @@ public enum Mixins {
     DISABLE_CREATIVE_TAB_ALL_SEARCH(new Builder("Disable the creative tab with search bar").setPhase(Phase.EARLY)
             .addMixinClasses("minecraft.MixinGuiContainerCreative").setSide(Side.CLIENT)
             .setApplyIf(() -> Common.config.removeCreativeSearchTab).addTargetedMod(TargetedMod.VANILLA)),
+    FIX_CHAT_COLOR_WRAPPING(new Builder("Fix wrapped chat lines missing colors").setPhase(Phase.EARLY)
+            .addMixinClasses("minecraft.MixinGuiNewChat_FixColorWrapping", "minecraft.FontRendererAccessor")
+            .setSide(Side.CLIENT).setApplyIf(() -> Common.config.fixChatWrappedColors)
+            .addTargetedMod(TargetedMod.VANILLA)),
     COMPACT_CHAT(new Builder("Compact chat").setPhase(Phase.EARLY)
             .addMixinClasses("minecraft.MixinGuiNewChat_CompactChat").setSide(Side.CLIENT)
             .setApplyIf(() -> Common.config.compactChat).addTargetedMod(TargetedMod.VANILLA)),
