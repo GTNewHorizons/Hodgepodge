48cf94056766abb571fdc19f351d0ff1ebf4f696
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-09-20 16:58:49 +0200

    Don't invert crosshair colors

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/forge/MixinGuiIngameForge_CrosshairThirdPerson.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/forge/MixinGuiIngameForge_CrosshairThirdPerson.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiIngameForge.class)
-public class MixinGuiIngameForge extends GuiIngame {
+public class MixinGuiIngameForge_CrosshairThirdPerson extends GuiIngame {
 
     @Inject(method = "renderCrosshairs", at = @At("HEAD"), cancellable = true, remap = false)
     public void hodgepodge$hideCrosshairThirdPerson(int width, int height, CallbackInfo ci) {
@@ -19,7 +19,7 @@ public class MixinGuiIngameForge extends GuiIngame {
         }
     }
 
-    private MixinGuiIngameForge(Minecraft mc) {
+    private MixinGuiIngameForge_CrosshairThirdPerson(Minecraft mc) {
         super(mc);
     }
 }
