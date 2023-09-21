8594c3aa95a25606d8a7ab385338a63187a46f35
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-09-21 13:39:56 +0200

    both handlers in MixinC01PacketChatMessage_LongerMessages can be merged into one

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinC01PacketChatMessage_LongerMessages.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinC01PacketChatMessage_LongerMessages.java
@@ -11,13 +11,8 @@ import com.mitchej123.hodgepodge.Common;
 @Mixin(C01PacketChatMessage.class)
 public class MixinC01PacketChatMessage_LongerMessages {
 
-    @ModifyConstant(method = "<init>(Ljava/lang/String;)V", constant = @Constant(intValue = 100))
-    public int hodgepodge$init_LongerMessages(int constant) {
-        return Common.config.messageLength;
-    }
-
-    @ModifyConstant(method = "readPacketData", constant = @Constant(intValue = 100))
-    public int hodgepodge$readPacketData_LongerMessages(int constant) {
+    @ModifyConstant(method = { "<init>(Ljava/lang/String;)V", "readPacketData" }, constant = @Constant(intValue = 100))
+    public int hodgepodge$LongerMessages(int constant) {
         return Common.config.messageLength;
     }
 }
