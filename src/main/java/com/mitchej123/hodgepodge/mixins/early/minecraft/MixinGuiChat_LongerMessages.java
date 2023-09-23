8ef3f03f06908b86b7f6f3b5add2d70c430706c4
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-09-24 00:07:39 +0200

    hardcode max sent message length to 256

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinGuiChat_LongerMessages.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinGuiChat_LongerMessages.java
@@ -6,13 +6,11 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-import com.mitchej123.hodgepodge.Common;
-
 @Mixin(GuiChat.class)
 public class MixinGuiChat_LongerMessages {
 
     @ModifyConstant(method = "initGui", constant = @Constant(intValue = 100))
     public int hodgepodge$LongerMessages(int constant) {
-        return Common.config.messageLength;
+        return 256;
     }
 }
