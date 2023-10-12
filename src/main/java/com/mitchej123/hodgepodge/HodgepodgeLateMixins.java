d1c621cc712f782684e79df88032320f902ebf63
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-10-12 12:54:40 +0200

    move getMixins method to Mixins enum

--- src/main/java/com/mitchej123/hodgepodge/HodgepodgeLateMixins.java
+++ src/main/java/com/mitchej123/hodgepodge/HodgepodgeLateMixins.java
@@ -1,7 +1,5 @@
 package com.mitchej123.hodgepodge;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -19,18 +17,6 @@ public class HodgepodgeLateMixins implements ILateMixinLoader {
 
     @Override
     public List<String> getMixins(Set<String> loadedMods) {
-        final List<String> mixins = new ArrayList<>();
-        final List<String> notLoading = new ArrayList<>();
-        for (Mixins mixin : Mixins.values()) {
-            if (mixin.phase == Mixins.Phase.LATE) {
-                if (mixin.shouldLoad(Collections.emptySet(), loadedMods)) {
-                    mixins.addAll(mixin.mixinClasses);
-                } else {
-                    notLoading.addAll(mixin.mixinClasses);
-                }
-            }
-        }
-        Common.log.info("Not loading the following LATE mixins: {}", notLoading.toString());
-        return mixins;
+        return Mixins.getLateMixins(loadedMods);
     }
 }
