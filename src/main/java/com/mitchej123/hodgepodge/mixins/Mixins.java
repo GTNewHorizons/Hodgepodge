50004339c7ec6608971d1ba74c49ce06d5cf7495
Caedis <Caedis@users.noreply.github.com>
2023-11-03 13:38:56 -0500

    Always have the packet size change on client and server
    The config option now just controls whether the client can send longer messages, which most people wont disable.

--- src/main/java/com/mitchej123/hodgepodge/mixins/Mixins.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/Mixins.java
@@ -86,9 +86,9 @@ public enum Mixins {
     LONGER_MESSAGES_CLIENT(new Builder("Longer Messages").setPhase(Phase.EARLY)
             .addMixinClasses("minecraft.MixinGuiChat_LongerMessages").setApplyIf(() -> Common.config.longerSentMessages)
             .addTargetedMod(TargetedMod.VANILLA).setSide(Side.CLIENT)),
-    LONGER_MESSAGES_SERVER(new Builder("Longer Messages").setPhase(Phase.EARLY)
+    LONGER_MESSAGES_SERVER(new Builder("Longer Messages Server Side").setPhase(Phase.EARLY)
             .addMixinClasses("minecraft.MixinC01PacketChatMessage_LongerMessages")
-            .setApplyIf(() -> Common.config.longerSentMessages).addTargetedMod(TargetedMod.VANILLA).setSide(Side.BOTH)),
+            .setApplyIf(() -> true).addTargetedMod(TargetedMod.VANILLA).setSide(Side.BOTH)), // always have this be enabled on the server
     SPEEDUP_GRASS_BLOCK_RANDOM_TICKING(new Builder("Speed up grass block random ticking").setPhase(Phase.EARLY)
             .addMixinClasses("minecraft.MixinBlockGrass").addTargetedMod(TargetedMod.VANILLA)
             .setApplyIf(() -> Common.config.speedupGrassBlockRandomTicking)),
