3059f47ea8aa29cb316cb7032bd956015701a8d4
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-11-15 03:10:43 +0100

    prevent spotless from having a heart attack

--- src/main/java/com/mitchej123/hodgepodge/asm/transformers/optifine/GLErrorLoggingTransformer.java
+++ src/main/java/com/mitchej123/hodgepodge/asm/transformers/optifine/GLErrorLoggingTransformer.java
@@ -9,7 +9,13 @@ import org.apache.logging.log4j.Logger;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.*;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.LabelNode;
+import org.objectweb.asm.tree.LocalVariableNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
 
 public class GLErrorLoggingTransformer implements IClassTransformer, Opcodes {
 
