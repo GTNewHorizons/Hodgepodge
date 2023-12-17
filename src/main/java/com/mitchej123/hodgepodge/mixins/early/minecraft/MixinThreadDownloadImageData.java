7387640e2f24b0f2f38ffad8973e4d4aba4ba1ac
kumquat-ir <66188216+kumquat-ir@users.noreply.github.com>
2023-12-16 21:20:43 -0500

    set user agent on image download requests

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinThreadDownloadImageData.java
@@ -0,0 +1,26 @@
+package com.mitchej123.hodgepodge.mixins.early.minecraft;
+
+import java.net.HttpURLConnection;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import com.mitchej123.hodgepodge.Tags;
+
+@Mixin(targets = "net/minecraft/client/renderer/ThreadDownloadImageData$1")
+public class MixinThreadDownloadImageData {
+
+    @Inject(
+            method = "run()V",
+            at = @At(value = "INVOKE", target = "Ljava/net/HttpURLConnection;setDoInput(Z)V"),
+            locals = LocalCapture.CAPTURE_FAILSOFT,
+            remap = false)
+    private void hodgepodge$injectRun(CallbackInfo ci, HttpURLConnection httpURLConnection) {
+        httpURLConnection.setRequestProperty(
+                "User-Agent",
+                "Minecraft/1.7.10 Hodgepodge/" + Tags.VERSION + " (+https://github.com/GTNewHorizons/Hodgepodge)");
+    }
+}
