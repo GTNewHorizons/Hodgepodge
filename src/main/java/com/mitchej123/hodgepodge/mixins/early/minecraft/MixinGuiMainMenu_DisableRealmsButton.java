5c7e859789a428b8e970c70e09cceee2372ba858
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-12-21 01:34:21 +0100

    completely hide realms button and make mods button wider to fill the gap

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinGuiMainMenu_DisableRealmsButton.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinGuiMainMenu_DisableRealmsButton.java
@@ -2,17 +2,26 @@ package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.gui.GuiScreen;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiMainMenu.class)
-public class MixinGuiMainMenu_DisableRealmsButton {
+public class MixinGuiMainMenu_DisableRealmsButton extends GuiScreen {
 
-    @ModifyVariable(method = "addSingleplayerMultiplayerButtons", at = @At(value = "STORE"), name = "realmsButton")
-    private GuiButton hodgepodge$disableRealmsButton(GuiButton realmsButton) {
-        realmsButton.enabled = false;
-        return realmsButton;
+    @Inject(method = "addSingleplayerMultiplayerButtons", at = @At("RETURN"))
+    private void hodgepodge$disableRealmsButton(int p_73969_1_, int p_73969_2_, CallbackInfo ci) {
+        for (Object o : this.buttonList) {
+            final GuiButton button = (GuiButton) o;
+            if (button.id == 14) { // realms button
+                button.visible = false;
+            } else if (button.id == 6) { // mods button
+                button.width = 200;
+                button.xPosition = this.width / 2 - 100;
+            }
+        }
     }
 }
