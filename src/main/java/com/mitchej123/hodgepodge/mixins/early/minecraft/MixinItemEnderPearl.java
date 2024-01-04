993bf0534f9c6ab5b1e7103567e30b60d52b9179
GitHub GTNH Actions <>
2024-01-04 01:19:26 +0000

    spotlessApply

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinItemEnderPearl.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinItemEnderPearl.java
@@ -1,15 +1,16 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
+import java.util.Random;
+
 import net.minecraft.entity.item.EntityEnderPearl;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemEnderPearl;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-
-import java.util.Random;
 
 @Mixin(ItemEnderPearl.class)
 public class MixinItemEnderPearl {
@@ -26,8 +27,7 @@ public class MixinItemEnderPearl {
         --itemStackIn.stackSize;
         worldIn.playSoundAtEntity(player, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
 
-        if (!worldIn.isRemote)
-        {
+        if (!worldIn.isRemote) {
             worldIn.spawnEntityInWorld(new EntityEnderPearl(worldIn, player));
         }
 
