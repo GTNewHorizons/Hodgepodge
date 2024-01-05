df99c3811c22c3c022666484d9e03d68e2148f2e
GTNH-Colen <54497873+GTNH-Colen@users.noreply.github.com>
2024-01-05 18:07:56 +0000

    Fixed

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinItemEnderPearl.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinItemEnderPearl.java
@@ -1,11 +1,9 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.PlayerCapabilities;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemEnderPearl;
 
-import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -15,14 +13,8 @@ public abstract class MixinItemEnderPearl extends Item {
 
     @Redirect(
             method = "onItemRightClick",
-            at = @At(
-                    value = "FIELD",
-                    target = "Lnet/minecraft/entity/player/EntityPlayer;capabilities:Lnet/minecraft/entity/player/PlayerCapabilities;",
-                    opcode = Opcodes.GETFIELD,
-                    ordinal = 0))
-    public PlayerCapabilities hodgepodge$getCapabilities(EntityPlayer player) {
-        PlayerCapabilities caps = player.capabilities;
-        caps.isCreativeMode = false;
-        return caps;
+            at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerCapabilities;isCreativeMode:Z"))
+    public boolean hodgepodge$removeCreativeCheck(PlayerCapabilities instance) {
+        return false;
     }
 }
