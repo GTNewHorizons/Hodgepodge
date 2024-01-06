b1a4bffdbbe4854364d9b22376ae8d709b39a049
Connor-Colenso <52056774+Connor-Colenso@users.noreply.github.com>
2024-01-06 17:48:28 +0000

    Revert "spotlessApply (#302)"
    This reverts commit dc6b468e8219c136df8a063f38a9d4a7131a708a.

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinItemEnderPearl.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinItemEnderPearl.java
@@ -1,16 +1,15 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
-import java.util.Random;
-
 import net.minecraft.entity.item.EntityEnderPearl;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemEnderPearl;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Random;
 
 @Mixin(ItemEnderPearl.class)
 public class MixinItemEnderPearl {
@@ -27,7 +26,8 @@ public class MixinItemEnderPearl {
         --itemStackIn.stackSize;
         worldIn.playSoundAtEntity(player, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
 
-        if (!worldIn.isRemote) {
+        if (!worldIn.isRemote)
+        {
             worldIn.spawnEntityInWorld(new EntityEnderPearl(worldIn, player));
         }
 
