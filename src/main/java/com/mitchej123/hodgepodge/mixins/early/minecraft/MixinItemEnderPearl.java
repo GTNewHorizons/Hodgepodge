f46e9bc50382bf34adc7b5ae64b0b7a2e9fcabc8
Connor-Colenso <52056774+Connor-Colenso@users.noreply.github.com>
2024-01-06 17:48:28 +0000

    Revert "Fixed"
    This reverts commit 0381fb58f92cdbfb75eaa783b30d4385a4dabfbc.

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinItemEnderPearl.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinItemEnderPearl.java
@@ -1,9 +1,11 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.PlayerCapabilities;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemEnderPearl;
 
+import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -13,8 +15,14 @@ public abstract class MixinItemEnderPearl extends Item {
 
     @Redirect(
             method = "onItemRightClick",
-            at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerCapabilities;isCreativeMode:Z"))
-    public boolean hodgepodge$removeCreativeCheck(PlayerCapabilities instance) {
-        return false;
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/entity/player/EntityPlayer;capabilities:Lnet/minecraft/entity/player/PlayerCapabilities;",
+                    opcode = Opcodes.GETFIELD,
+                    ordinal = 0))
+    public PlayerCapabilities hodgepodge$getCapabilities(EntityPlayer player) {
+        PlayerCapabilities caps = player.capabilities;
+        caps.isCreativeMode = false;
+        return caps;
     }
 }
