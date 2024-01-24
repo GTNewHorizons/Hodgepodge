3e3937964762b6ea0e8a19631872ec7d7e750823
UltraHex <matthew.william.noel@gmail.com>
2024-01-23 16:08:22 -0800

    Remove star import

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinRenderGlobal_Pass0Blend.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinRenderGlobal_Pass0Blend.java
@@ -5,7 +5,10 @@ import net.minecraft.client.renderer.RenderGlobal;
 
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderGlobal.class)
