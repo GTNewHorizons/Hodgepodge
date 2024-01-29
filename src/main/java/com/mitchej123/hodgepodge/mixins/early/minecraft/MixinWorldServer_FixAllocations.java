3f4ad47e0f4e455e95c473ae5bd02749c38add3f
GitHub GTNH Actions <>
2024-01-29 17:34:45 +0000

    spotlessApply

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinWorldServer_FixAllocations.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinWorldServer_FixAllocations.java
@@ -1,20 +1,22 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
-import com.mitchej123.hodgepodge.hax.LongChunkCoordIntPairSet;
-import net.minecraft.client.multiplayer.WorldClient;
+import java.util.Iterator;
+import java.util.Set;
+
 import net.minecraft.world.WorldServer;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.Iterator;
-import java.util.Set;
+import com.mitchej123.hodgepodge.hax.LongChunkCoordIntPairSet;
 
 @Mixin(WorldServer.class)
 public abstract class MixinWorldServer_FixAllocations {
 
-
-    @Redirect(method="func_147456_g", at = @At(value = "INVOKE", target = "Ljava/util/Set;iterator()Ljava/util/Iterator;"))
+    @Redirect(
+            method = "func_147456_g",
+            at = @At(value = "INVOKE", target = "Ljava/util/Set;iterator()Ljava/util/Iterator;"))
     private Iterator<?> fixAllocations(Set instance) {
         return ((LongChunkCoordIntPairSet) instance).unsafeIterator();
     }
