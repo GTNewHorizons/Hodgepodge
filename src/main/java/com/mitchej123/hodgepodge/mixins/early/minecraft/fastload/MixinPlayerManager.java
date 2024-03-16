3f9240fb073291f8448fe5bc011dd39e6335e8cf
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-03-16 05:32:43 -0400

    Catch more worldgen

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/fastload/MixinPlayerManager.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/fastload/MixinPlayerManager.java
@@ -14,8 +14,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Coerce;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.List;
 
@@ -23,6 +21,39 @@ import java.util.List;
 public class MixinPlayerManager {
 
     @Shadow @Final private WorldServer theWorldServer;
+
+    private static final ChunkCoordIntPair hodgepodge$dummyCoord = new ChunkCoordIntPair(Integer.MAX_VALUE, Integer.MAX_VALUE);
+
+    @WrapOperation(
+            method = "filterChunkLoadQueue",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/management/PlayerManager;getOrCreateChunkWatcher(IIZ)Lnet/minecraft/server/management/PlayerManager$PlayerInstance;",
+                    ordinal = 1))
+    private PlayerManager.PlayerInstance hodgepodge$throttleChunkGen(PlayerManager instance, int cx, int cz, boolean instantiate, Operation<PlayerManager.PlayerInstance> original) {
+
+        final long key = ChunkCoordIntPair.chunkXZ2Int(cx, cz);
+
+        if (((FastCPS) this.theWorldServer.theChunkProviderServer).doesChunkExist(cx, cz, key))
+            return original.call(instance, cx, cz, instantiate); // Always load generated chunks
+        if (((FastWorldServer) this.theWorldServer).isThrottlingGen())
+            return null; // Don't generate new chunks while throttling
+
+        // Generate, but count it against the budget this tick
+        ((FastWorldServer) this.theWorldServer).spendGenBudget(1);
+        return original.call(instance, cx, cz, instantiate);
+    }
+
+    @WrapOperation(
+            method = "filterChunkLoadQueue",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/server/management/PlayerManager$PlayerInstance;chunkLocation:Lnet/minecraft/world/ChunkCoordIntPair;",
+                    ordinal = 1))
+    private ChunkCoordIntPair hodgepodge$throttleChunkGen(PlayerManager.PlayerInstance instance, Operation<ChunkCoordIntPair> original) {
+        if (instance != null) return original.call(instance);
+        return hodgepodge$dummyCoord;
+    }
 
     @WrapWithCondition(method = "updatePlayerPertinentChunks", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
     private boolean hodgepodge$throttleChunkGen(
