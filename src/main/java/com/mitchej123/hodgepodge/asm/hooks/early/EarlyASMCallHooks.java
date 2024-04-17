f261167e3f3c36e42eac0332c1622e2a3dd2c1d0
Raven Szewczyk <git@eigenraven.me>
2024-04-17 22:35:16 +0100

    Fix egregious memory usage of Forge's Configuration class (#357)
    * Add an RFB plugin optimizing memory used by Forge's Configuration objects
    
    * Reduce memory usage further
    
    * Add doc comment to the transformer

--- src/main/java/com/mitchej123/hodgepodge/asm/hooks/early/EarlyASMCallHooks.java
+++ src/main/java/com/mitchej123/hodgepodge/asm/hooks/early/EarlyASMCallHooks.java
@@ -30,4 +30,49 @@ public class EarlyASMCallHooks {
         }
         return null;
     }
+
+    private static final String INT_MIN_STRING = Integer.toString(Integer.MIN_VALUE);
+    private static final String INT_MAX_STRING = Integer.toString(Integer.MAX_VALUE);
+    private static final String DOUBLE_MAX_STRING = Double.toString(Double.MAX_VALUE);
+    private static final String DOUBLE_NEG_MAX_STRING = Double.toString(-Double.MAX_VALUE);
+
+    public static String intToCachedString(int value) {
+        return switch (value) {
+            case Integer.MIN_VALUE -> INT_MIN_STRING;
+            case Integer.MAX_VALUE -> INT_MAX_STRING;
+            case 0 -> "0";
+            case 1 -> "1";
+            case 2 -> "2";
+            case 3 -> "3";
+            case 4 -> "4";
+            case 5 -> "5";
+            case 10 -> "10";
+            case 100 -> "100";
+            default -> String.valueOf(value).intern();
+        };
+    }
+
+    public static String doubleToCachedString(double value) {
+        if (value == Double.MAX_VALUE) {
+            return DOUBLE_MAX_STRING;
+        } else if (value == -Double.MAX_VALUE) {
+            return DOUBLE_NEG_MAX_STRING;
+        }
+        return String.valueOf(value).intern();
+    }
+
+    private static final String[] EMPTY_STRING_ARRAY = new String[0];
+
+    public static String[] internArray(String[] array) {
+        if (array == null) {
+            return null;
+        }
+        if (array.length == 0) {
+            return EMPTY_STRING_ARRAY;
+        }
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (array[i] == null) ? null : array[i].intern();
+        }
+        return array;
+    }
 }
