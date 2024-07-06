8b3d205b0c1440e7b8c102007097554cff12ec96
Mary Hopson <doyoumined@gmail.com>
2024-07-05 22:07:03 -0400

    Change to see if this mixin is even being loaded

--- src/main/java/com/mitchej123/hodgepodge/mixins/late/extrautilities/MixinFluidBufferRetrieval.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/extrautilities/MixinFluidBufferRetrieval.java
@@ -25,7 +25,7 @@ public class MixinFluidBufferRetrieval extends FluidBuffer {
     public boolean transfer(TileEntity tile, ForgeDirection side, IPipe insertingPipe, int x, int y, int z,
             ForgeDirection travelDir) {
         if (tile instanceof IFluidHandler destTank) {
-            int drainmax = this.node.getNode().upgradeNo(3) == 0 ? 200 : this.tank.getCapacity();
+            int drainmax = this.node.getNode().upgradeNo(3) == 0 ? 500 : this.tank.getCapacity();
             int drainable = ((TileEntityRetrievalNodeLiquid) this.node.getNode())
                     .fill(this.node.getNodeDir(), destTank.drain(side, drainmax, false), false);
             return drainable <= 0 || ((TileEntityRetrievalNodeLiquid) this.node.getNode())
