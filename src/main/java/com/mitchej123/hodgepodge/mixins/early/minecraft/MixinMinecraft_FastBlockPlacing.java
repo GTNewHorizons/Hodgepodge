ab0b04a2a04e076758ca591f0712a8ae4d7be890
Caedis <Caedis@users.noreply.github.com>
2024-07-13 17:02:30 -0500

    Remove all allocations from FastBlockPlacing (#392)

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinMinecraft_FastBlockPlacing.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinMinecraft_FastBlockPlacing.java
@@ -2,13 +2,12 @@ package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityClientPlayerMP;
-import net.minecraft.client.settings.GameSettings;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.MovingObjectPosition;
-import net.minecraft.util.Vec3;
 import net.minecraftforge.common.util.ForgeDirection;
 
+import org.joml.Vector3i;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -27,11 +26,13 @@ public class MixinMinecraft_FastBlockPlacing {
     public MovingObjectPosition objectMouseOver;
     @Shadow
     public EntityClientPlayerMP thePlayer;
-    @Shadow
-    public GameSettings gameSettings;
 
     @Unique
-    private Vec3 lastPosition;
+    private final Vector3i currentPosition = new Vector3i(0, 0, 0);
+    @Unique
+    private final Vector3i comparePosition = new Vector3i(0, 0, 0);
+    @Unique
+    private final Vector3i lastPosition = new Vector3i(0, 0, 0);
     @Unique
     private ForgeDirection lastSide;
 
@@ -53,37 +54,17 @@ public class MixinMinecraft_FastBlockPlacing {
         if (itemstack == null) return;
         if (!(itemstack.getItem() instanceof ItemBlock)) return;
 
-        Vec3 pos = createVec3(objectMouseOver);
-        if (rightClickDelayTimer > 0 && !isPosEqual(pos, lastPosition)
-                && (lastPosition == null || !isPosEqual(pos, getNewPosition(lastPosition, lastSide)))) {
+        currentPosition.set(objectMouseOver.blockX, objectMouseOver.blockY, objectMouseOver.blockZ);
+        if (rightClickDelayTimer > 0 && !currentPosition.equals(lastPosition)
+                && (!currentPosition.equals(
+                        comparePosition.set(lastPosition).add(lastSide.offsetX, lastSide.offsetY, lastSide.offsetZ)))) {
             rightClickDelayTimer = 0;
-        } else if (rightClickDelayTimer == 0 && isPosEqual(pos, lastPosition)
+        } else if (rightClickDelayTimer == 0 && currentPosition.equals(lastPosition)
                 && lastSide.equals(ForgeDirection.getOrientation(objectMouseOver.sideHit))) {
                     rightClickDelayTimer = 4;
                 }
 
-        lastPosition = pos;
+        lastPosition.set(currentPosition);
         lastSide = ForgeDirection.getOrientation(objectMouseOver.sideHit);
-    }
-
-    @Unique
-    private Vec3 createVec3(MovingObjectPosition pos) {
-        return Vec3.createVectorHelper(pos.blockX, pos.blockY, pos.blockZ);
-    }
-
-    @Unique
-    private boolean isPosEqual(Vec3 p1, Vec3 p2) {
-        if (p1 == null || p2 == null) {
-            return false;
-        }
-        return p1.xCoord == p2.xCoord && p1.yCoord == p2.yCoord && p1.zCoord == p2.zCoord;
-    }
-
-    @Unique
-    private Vec3 getNewPosition(Vec3 pos, ForgeDirection direction) {
-        return Vec3.createVectorHelper(
-                pos.xCoord + direction.offsetX,
-                pos.yCoord + direction.offsetY,
-                pos.zCoord + direction.offsetZ);
     }
 }
