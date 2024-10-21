3b3a76e5de4cb78247298c52c17d238052a8d22c
Lyft <127234178+Lyfts@users.noreply.github.com>
2024-10-21 18:41:38 +0200

    Add a search bar to the mod config gui (#427)
    * add a search bar to the mod config gui
    
    * move `@Config.RequiresMcRestart` onto class
    
    * enable categorized for config gui
    
    * move the rest of the @Config.RequiresMcRestart annotations onto class
    
    * oops wrong one
    
    * removed unused shadow
    
    * spotlessApply (#430)
    
    Co-authored-by: GitHub GTNH Actions <>
    
    ---------
    
    Co-authored-by: Martin Robertz <dream-master@gmx.net>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Caedis <Caedis@users.noreply.github.com>

--- src/main/java/com/mitchej123/hodgepodge/config/PollutionRecolorConfig.java
+++ src/main/java/com/mitchej123/hodgepodge/config/PollutionRecolorConfig.java
@@ -3,33 +3,29 @@ package com.mitchej123.hodgepodge.config;
 import com.gtnewhorizon.gtnhlib.config.Config;
 
 @Config(modid = "hodgepodge", category = "pollution_recolor")
+@Config.RequiresMcRestart
 public class PollutionRecolorConfig {
 
     @Config.Comment("Changes colors of certain blocks based on pollution levels")
     @Config.DefaultBoolean(true)
-    @Config.RequiresMcRestart
     public static boolean pollutionBlockRecolor;
 
     @Config.Comment("Double Plant Blocks - Recolor Block List")
     @Config.DefaultStringList({ "net.minecraft.block.BlockDoublePlant:FLOWER", })
-    @Config.RequiresMcRestart
     public static String[] renderBlockDoublePlant;
 
     @Config.Comment("Liquid Blocks - Recolor Block List")
     @Config.DefaultStringList({ "net.minecraft.block.BlockLiquid:LIQUID" })
-    @Config.RequiresMcRestart
     public static String[] renderBlockLiquid;
 
     @Config.Comment("Block Vine - Recolor Block List")
     @Config.DefaultStringList({ "net.minecraft.block.BlockVine:FLOWER", })
-    @Config.RequiresMcRestart
     public static String[] renderblockVine;
 
     @Config.Comment("Crossed Squares - Recolor Block List")
     @Config.DefaultStringList({ "net.minecraft.block.BlockTallGrass:FLOWER", "net.minecraft.block.BlockFlower:FLOWER",
             "biomesoplenty.common.blocks.BlockBOPFlower:FLOWER", "biomesoplenty.common.blocks.BlockBOPFlower2:FLOWER",
             "biomesoplenty.common.blocks.BlockBOPFoliage:FLOWER", })
-    @Config.RequiresMcRestart
     public static String[] renderCrossedSquares;
 
     @Config.Comment("Standard Blocks - Recolor Block List")
@@ -37,7 +33,6 @@ public class PollutionRecolorConfig {
             "biomesoplenty.common.blocks.BlockOriginGrass:GRASS", "biomesoplenty.common.blocks.BlockLongGrass:GRASS",
             "biomesoplenty.common.blocks.BlockNewGrass:GRASS", "tconstruct.blocks.slime.SlimeGrass:GRASS",
             "thaumcraft.common.blocks.BlockMagicalLeaves:LEAVES", })
-    @Config.RequiresMcRestart
     public static String[] renderStandardBlock;
 
 }
