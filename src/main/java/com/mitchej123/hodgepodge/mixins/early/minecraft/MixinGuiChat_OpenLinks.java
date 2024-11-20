504bc026c1dc68d2086c357355035fb92420ff7a
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2024-11-20 09:08:11 +0100

    use inject at head instead

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinGuiChat_OpenLinks.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinGuiChat_OpenLinks.java
@@ -5,20 +5,20 @@ import java.net.URI;
 import net.minecraft.client.gui.GuiChat;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.gtnewhorizon.gtnhlib.util.FilesUtil;
 
 @Mixin(GuiChat.class)
 public class MixinGuiChat_OpenLinks {
 
-    /**
-     * @author Alexdoru
-     * @reason The Vanilla method doesn't work on some OS
-     */
-    @Overwrite
-    protected void func_146407_a(URI uri) {
+    // @reason The Vanilla method doesn't work on some OS
+    @Inject(method = "func_146407_a", at = @At("HEAD"), cancellable = true)
+    private void hodgepodge$fixFileOpening(URI uri, CallbackInfo ci) {
         FilesUtil.openUri(uri);
+        ci.cancel();
     }
 
 }
