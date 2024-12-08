0369476bf8f5f8787b12333627c377ae975bf4cb
Midnight <23142346+Midnight145@users.noreply.github.com>
2024-12-07 22:38:36 -0600

    spotless

--- src/main/java/com/mitchej123/hodgepodge/mixins/late/extrautilities/MixinTileEntityEnderQuarry.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/extrautilities/MixinTileEntityEnderQuarry.java
@@ -1,10 +1,10 @@
 package com.mitchej123.hodgepodge.mixins.late.extrautilities;
 
-import com.mitchej123.hodgepodge.config.TweaksConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
+import com.mitchej123.hodgepodge.config.TweaksConfig;
 import com.rwtema.extrautils.tileentity.enderquarry.TileEntityEnderQuarry;
 
 @Mixin(value = TileEntityEnderQuarry.class, remap = false)
