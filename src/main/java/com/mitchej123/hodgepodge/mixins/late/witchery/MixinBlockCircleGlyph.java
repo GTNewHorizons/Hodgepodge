34a04394dc5e28092bac09e620899ce1ae3d0a3c
Darren Eberly <darren.eberly@proton.me>
2024-12-09 16:02:43 -0500

    Fix off by one int clamp in Witchery's glyph rendering

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/witchery/MixinBlockCircleGlyph.java
@@ -0,0 +1,21 @@
+package com.mitchej123.hodgepodge.mixins.late.witchery;
+
+import com.emoniph.witchery.blocks.BlockCircleGlyph;
+import net.minecraft.util.IIcon;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(BlockCircleGlyph.class)
+public class MixinBlockCircleGlyph {
+
+    @Shadow(remap = false)
+    private IIcon[] icons;
+
+    @ModifyConstant(method = "getIcon(II)Lnet/minecraft/util/IIcon;", constant = @Constant(intValue = 12))
+    private int hodgepodge$fixMetadataClamp(int oldValue) {
+        return icons.length - 1;
+    }
+
+}
