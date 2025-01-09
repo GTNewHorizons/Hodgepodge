bddbe2d49140af2e5f8d8b63528bddb280cfe748
Caedis <Caedis@users.noreply.github.com>
2025-01-09 15:22:33 -0600

    Backport 1.20's 'pause-when-empty-seconds' server property

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/server/MixinMinecraftServer_PauseWhenEmpty.java
@@ -0,0 +1,64 @@
+package com.mitchej123.hodgepodge.mixins.early.minecraft.server;
+
+import com.mitchej123.hodgepodge.Common;
+import com.mitchej123.hodgepodge.Hodgepodge;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.server.dedicated.PropertyManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.logging.ILogger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.io.File;
+import java.net.Proxy;
+
+@Mixin(DedicatedServer.class)
+public abstract class MixinMinecraftServer_PauseWhenEmpty extends MinecraftServer {
+    @Shadow private PropertyManager settings;
+
+    @Unique
+    private int hodgepodge$pauseWhenEmptySeconds = 0;
+    @Unique
+    private int hodgepodge$emptyTicks = 0;
+
+    public MixinMinecraftServer_PauseWhenEmpty(File workDir, Proxy proxy) {
+        super(workDir, proxy);
+    }
+
+    @Inject(method = "startServer", at = @At(value = "INVOKE", target = "Lcpw/mods/fml/common/FMLCommonHandler;onServerStarted()V", shift = At.Shift.AFTER))
+    public void hodgepodge$setupServer(CallbackInfoReturnable<Boolean> cir){
+        hodgepodge$pauseWhenEmptySeconds = settings.getIntProperty("pause-when-empty-seconds", 0);
+    }
+
+    @Override
+    public void tick(){
+        int pauseTicks = hodgepodge$pauseWhenEmptySeconds * 20;
+        if (pauseTicks > 0){
+            if (getCurrentPlayerCount() == 0){
+                this.hodgepodge$emptyTicks++;
+            } else {
+                this.hodgepodge$emptyTicks = 0;
+            }
+
+            if (hodgepodge$emptyTicks >= pauseTicks){
+                if (hodgepodge$emptyTicks == pauseTicks){
+                    Common.log.info("Server empty for {} seconds, saving and pausing", hodgepodge$pauseWhenEmptySeconds);
+                    this.serverConfigManager.saveAllPlayerData();
+                    this.saveAllWorlds(true);
+                }
+
+                // to process new connections
+                this.func_147137_ag().networkTick();
+                return;
+            }
+        }
+
+        super.tick();
+    }
+}
