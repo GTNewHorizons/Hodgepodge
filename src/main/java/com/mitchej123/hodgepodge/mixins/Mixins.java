5263eaec57f30dc0abff09bcb8f6cfa0be7b3dfd
raylras <raylras@outlook.com>
2025-02-08 06:43:09 +0800

    fix entity attributes range

--- src/main/java/com/mitchej123/hodgepodge/mixins/Mixins.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/Mixins.java
@@ -115,6 +115,10 @@ public enum Mixins {
     TRANSPARENT_CHAT(new Builder("Transparent Chat").setPhase(Phase.EARLY)
             .addMixinClasses("minecraft.MixinGuiNewChat_TransparentChat").setSide(Side.CLIENT)
             .setApplyIf(() -> TweaksConfig.transparentChat).addTargetedMod(TargetedMod.VANILLA)),
+    FIX_ENTITY_ATTRIBUTES_RANGE(new Builder("Fix Entity Attributes Range").setPhase(Phase.EARLY)
+            .addMixinClasses("minecraft.MixinNetHandlerPlayClient_FixEntityAttributesRange").setSide(Side.CLIENT)
+            .setApplyIf(() -> FixesConfig.fixEntityAttributesRange).addTargetedMod(TargetedMod.VANILLA)),
+
     // config handled in mixin due to server->client config sync
     LONGER_MESSAGES_CLIENT(new Builder("Longer Messages Client Side").setPhase(Phase.EARLY)
             .addMixinClasses("minecraft.MixinGuiChat_LongerMessages").setApplyIf(() -> true)
