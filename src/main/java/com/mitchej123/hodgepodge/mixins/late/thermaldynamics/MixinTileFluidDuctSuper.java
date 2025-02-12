871325f796ad1020aef2975a78f7442679881f71
DrParadox7 <gustavo.caliari@live.com>
2025-02-12 16:39:48 +0100

    Prevent CastClassException on invalid duct network
    Cancels and destroys attempts to create invalid fluid duct networks instead of crashing

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/thermaldynamics/MixinTileFluidDuctSuper.java
@@ -0,0 +1,22 @@
+package com.mitchej123.hodgepodge.mixins.late.thermaldynamics;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import cofh.thermaldynamics.duct.fluid.FluidGridSuper;
+import cofh.thermaldynamics.duct.fluid.TileFluidDuctSuper;
+import cofh.thermaldynamics.multiblock.MultiBlockGrid;
+
+@Mixin(TileFluidDuctSuper.class)
+public class MixinTileFluidDuctSuper {
+
+    @Inject(method = "setGrid", at = @At(value = "HEAD"), remap = false, cancellable = true)
+    public void hodgepodge$setGrid(MultiBlockGrid par1, CallbackInfo ci) {
+        if (!(par1 instanceof FluidGridSuper)) {
+            par1.destroy();
+            ci.cancel();
+        }
+    }
+}
