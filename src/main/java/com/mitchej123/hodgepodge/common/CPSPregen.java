f6d9c31df08c8c42a76cbd273a4f35c302893809
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-03-15 17:01:55 -0400

    Still broken, but takes a little longer

--- src/main/java/com/mitchej123/hodgepodge/common/CPSPregen.java
+++ src/main/java/com/mitchej123/hodgepodge/common/CPSPregen.java
@@ -1,8 +1,6 @@
 package com.mitchej123.hodgepodge.common;
 
 import static com.mitchej123.hodgepodge.Common.log;
-import static com.mitchej123.hodgepodge.util.ChunkPosUtil.getPackedX;
-import static com.mitchej123.hodgepodge.util.ChunkPosUtil.getPackedZ;
 import static com.mitchej123.hodgepodge.util.ChunkPosUtil.toLong;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
@@ -11,10 +9,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -24,19 +18,27 @@ import net.minecraftforge.common.DimensionManager;
 
 public class CPSPregen {
     private static final Int2ObjectOpenHashMap<CPSPregen> PREGENERATORS = new Int2ObjectOpenHashMap<>();
+    private static final int N_WORKERS = 4;
+    private static final int INTERVAL = 25;
 
     private final WorldProvider provider;
     private final AnvilChunkLoader chunkLoader;
     private final ChunkProviderServer cps;
+    private final ExecutorService[] workers = new ExecutorService[N_WORKERS];
+    private final IChunkProvider[] chunkProviders = new IChunkProvider[N_WORKERS];
     private final ExecutorService executor = Executors.newFixedThreadPool(4);
-    private final ObjectArrayList<IChunkProvider> generators = new ObjectArrayList<>();
-    private final Semaphore lock = new Semaphore(1);
-    private final ObjectArrayList<CompletableFuture<CGTask>> tasks = new ObjectArrayList<>();
+    //private final ObjectArrayList<IChunkProvider> generators = new ObjectArrayList<>();
+    //private final Semaphore lock = new Semaphore(1);
+    private final ObjectArrayList<CGFuture> tasks = new ObjectArrayList<>();
 
     public CPSPregen(WorldProvider provider, AnvilChunkLoader chunkLoader, ChunkProviderServer cps) {
         this.provider = provider;
         this.chunkLoader = chunkLoader;
         this.cps = cps;
+
+        for (int i = 0; i < N_WORKERS; i++) {
+            workers[i] = Executors.newSingleThreadExecutor();
+        }
     }
 
     public static CPSPregen getPregenerator(int dimID) {
@@ -61,59 +63,61 @@ public class CPSPregen {
     public int generateChunksBlocking(int cxl, int czl, int cxh, int czh) {
         for (int x = cxl; x <= cxh; x++) {
             for (int z = czl; z <= czh; z++) {
-                if (chunkLoader.chunkExists(null, x, z)) continue;
-                while (!lock.tryAcquire()) { Thread.yield(); }
-                var gen = generators.isEmpty() ? provider.createChunkGenerator() : generators.pop();
-                lock.release();
-                tasks.add(submitChunk(x, z, gen));
+                if (cps.chunkExists(x, z) || chunkLoader.chunkExists(null, x, z)) continue;
+
+                final int cx = x;
+                final int cz = z;
+                final var t = new CGFuture(x, z);
+                final var gen = provider.createChunkGenerator();
+                tasks.add(t);
+                executor.submit(() -> t.complete(gen.provideChunk(cx, cz)));
             }
         }
 
         // Decorate and load the chunks - hope you have an aggressive unloader!
         int numGenerated = 0;
-        for (int i = 0; i < tasks.size(); i++) {
-            var task = tasks.get(i);
+        boolean skip = false;
+        var len = tasks.size();
+        for (int i = 0; i < len; i++) {
+            var task = tasks.pop();
             try {
-                var result = task.get(5, TimeUnit.MILLISECONDS);
-
-                if (cps.loadedChunkHashMap.containsItem(result.pos)) continue;
-
-                cps.loadedChunkHashMap.add(result.pos, result.chunk);
-                cps.loadedChunks.add(result.chunk);
-                result.chunk.onChunkLoad();
-                result.chunk.populateChunk(cps, cps, getPackedX(result.pos), getPackedZ(result.pos));
                 numGenerated++;
+                //if (numGenerated % INTERVAL == 0) {
+                    log.info("Processed {} chunks", numGenerated);
+                //}
+
+                if (cps.chunkExists(task.cx, task.cz)) continue;
+
+                var result = task.get(/** /5, TimeUnit.MILLISECONDS/**/);
+                cps.loadedChunkHashMap.add(task.pos, result);
+                cps.loadedChunks.add(result);
+                result.onChunkLoad();
+                result.populateChunk(cps, cps, task.cx, task.cz);
             } catch (InterruptedException ignored) {
             } catch (ExecutionException e) {
                 throw new RuntimeException(e);
-            } catch (TimeoutException e) {
-                log.warn("Timed out while waiting for chunk generation - running through again!");
-                executor.shutdownNow();
-                numGenerated += generateChunksBlocking(cxl, czl, cxh, czh);
-            }
+            }/** / catch (TimeoutException e) {
+                log.warn("Timed out while waiting for chunk generation - skipping chunk!");
+                skip = true;
+            }//*/
         }
 
+        if (skip) {
+            log.warn("Going around again!");
+            numGenerated += generateChunksBlocking(cxl, czl, cxh, czh);
+        }
         return numGenerated;
     }
 
-    private CompletableFuture<CGTask> submitChunk(int cx, int cz, IChunkProvider gen) {
-        var ret = new CompletableFuture<CGTask>();
-        try {
-            executor.submit(() -> {
-                var undec = gen.provideChunk(cx, cz);
-                ret.complete(new CGTask(undec, toLong(cx, cz)));
-            });
-        } catch (RejectedExecutionException ignored) {}
-        return ret;
-    }
-
-    private static class CGTask {
-        private final Chunk chunk;
+    private static class CGFuture extends CompletableFuture<Chunk> {
+        private final int cx;
+        private final int cz;
         private final long pos;
 
-        private CGTask(Chunk c, long pos) {
-            chunk = c;
-            this.pos = pos;
+        private CGFuture(int cx, int cz) {
+            this.cx = cx;
+            this.cz = cz;
+            this.pos = toLong(cx, cz);
         }
     }
 }
