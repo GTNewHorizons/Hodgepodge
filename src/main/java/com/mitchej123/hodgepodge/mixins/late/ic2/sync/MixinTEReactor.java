9bab4663877e8ad976a38f8043c4b8a384ac92e8
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-03-24 01:26:17 -0400

    spotless :fail:

--- src/main/java/com/mitchej123/hodgepodge/mixins/late/ic2/sync/MixinTEReactor.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/ic2/sync/MixinTEReactor.java
@@ -2,18 +2,27 @@ package com.mitchej123.hodgepodge.mixins.late.ic2.sync;
 
 import static java.lang.Math.floorMod;
 
-import ic2.core.block.reactor.tileentity.TileEntityNuclearReactorElectric;
 import net.minecraft.world.World;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import ic2.core.block.reactor.tileentity.TileEntityNuclearReactorElectric;
+
 @Mixin(value = TileEntityNuclearReactorElectric.class, remap = false)
 public abstract class MixinTEReactor {
-    @Shadow public abstract World getWorld();
 
-    @Redirect(method = "updateEntityServer", at = @At(value = "FIELD", target = "Lic2/core/block/reactor/tileentity/TileEntityNuclearReactorElectric;updateTicker:I", ordinal = 0))
+    @Shadow
+    public abstract World getWorld();
+
+    @Redirect(
+            method = "updateEntityServer",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lic2/core/block/reactor/tileentity/TileEntityNuclearReactorElectric;updateTicker:I",
+                    ordinal = 0))
     private int hodgepodge$sync(TileEntityNuclearReactorElectric instance) {
         // Ensure the update ticker is locked to the world time, using a modulo to never be negative.
         // Technically, the IC2 one would roll over every ~3.5 years, but that means it's possible that
