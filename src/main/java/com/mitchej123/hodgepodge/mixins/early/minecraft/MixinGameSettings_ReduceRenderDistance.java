e42bf36dac2f4e518b0394e6eebd94addd071574
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-04-27 19:33:45 -0400

    Reduce render distance w/o an extender
    Without this, Minecraft crashes when render distance was set high, then the render dist extender is uninstalled.

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinGameSettings_ReduceRenderDistance.java
@@ -0,0 +1,17 @@
+package com.mitchej123.hodgepodge.mixins.early.minecraft;
+
+import static java.lang.Math.min;
+
+import net.minecraft.client.settings.GameSettings;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(GameSettings.class)
+public class MixinGameSettings_ReduceRenderDistance {
+
+    @Redirect(method = "loadOptions", at = @At(value = "FIELD", target = "Lnet/minecraft/client/settings/GameSettings;renderDistanceChunks:I"))
+    public void hodgepodge$fixOptifineChunkLoadingCrash(GameSettings instance, int value) {
+        instance.renderDistanceChunks = min(16, value);
+    }
+}
