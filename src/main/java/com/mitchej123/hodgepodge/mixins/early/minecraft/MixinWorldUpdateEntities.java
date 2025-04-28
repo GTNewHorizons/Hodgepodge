f4e71db982960f448fee03068153f25db0c3fe53
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-04-28 13:51:02 -0400

    Change to reference hashmap

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinWorldUpdateEntities.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinWorldUpdateEntities.java
@@ -1,15 +1,12 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
+import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
 import java.util.Collection;
 import java.util.List;
-
 import net.minecraft.world.World;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-
-import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 
 @Mixin(World.class)
 public class MixinWorldUpdateEntities {
@@ -19,6 +16,6 @@ public class MixinWorldUpdateEntities {
             at = @At(value = "INVOKE", target = "Ljava/util/List;removeAll(Ljava/util/Collection;)Z"))
     public <E> boolean fasterRemoveAll(List<E> targetList, Collection<E> collectionToRemove) {
         // Borrowed from Forge for 1.12.2 -- forge: faster "contains" makes this removal much more efficient
-        return targetList.removeAll(new ObjectOpenHashSet<>(collectionToRemove));
+        return targetList.removeAll(new ReferenceOpenHashSet<>(collectionToRemove));
     }
 }
