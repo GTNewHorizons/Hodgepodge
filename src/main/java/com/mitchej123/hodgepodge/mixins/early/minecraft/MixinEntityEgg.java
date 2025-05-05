6b879f172348a08602518e8ac721fee1e1872478
jack <jack>
2025-05-05 11:36:01 +0200

    Added same velocity as in the vanilla fix, instead of snowball's velocity

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinEntityEgg.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinEntityEgg.java
@@ -1,19 +1,34 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
 import net.minecraft.entity.projectile.EntityEgg;
+import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(EntityEgg.class)
-public abstract class MixinEntityEgg {
+public abstract class MixinEntityEgg extends EntityThrowable {
 
-    @ModifyArg(
+    public MixinEntityEgg(World p_i1776_1_) {
+        super(p_i1776_1_);
+    }
+
+    @Redirect(
             method = "onImpact",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;spawnParticle(Ljava/lang/String;DDDDDD)V"),
-            index = 0)
-    private String replaceParticle(String originalParticle) {
-        return "eggpoof";
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;spawnParticle(Ljava/lang/String;DDDDDD)V"))
+    private void replaceSpawnParticle(World world, String particleName, double x, double y, double z, double velocityX,
+            double velocityY, double velocityZ) {
+        worldObj.spawnParticle(
+                "iconcrack_" + Item.getIdFromItem(Items.egg),
+                x,
+                y,
+                z,
+                ((double) rand.nextFloat() - 0.5D) * 0.08D,
+                ((double) rand.nextFloat() - 0.5D) * 0.08D,
+                ((double) rand.nextFloat() - 0.5D) * 0.08D);
     }
 }
