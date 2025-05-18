d26efd253e3a5990f2dc9879163cf41e3bea83a0
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-05-18 03:28:14 -0400

    SPOTLES

--- src/main/java/com/mitchej123/hodgepodge/mixins/Mixins.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/Mixins.java
@@ -573,9 +573,8 @@ public enum Mixins implements IMixins {
             .addMixinClasses("minecraft.fastload.MixinEntityPlayerMP", "minecraft.fastload.MixinChunkProviderServer")
             .setApplyIf(() -> SpeedupsConfig.fastChunkHandling)),
 
-    CANCEL_NONE_SOUNDS(new MixinBuilder("Skips playing 'none' sounds").setPhase(Phase.EARLY)
-            .setSide(Side.BOTH).addTargetedMod(TargetedMod.VANILLA)
-            .addMixinClasses("minecraft.shutup.MixinEntityLiving")
+    CANCEL_NONE_SOUNDS(new MixinBuilder("Skips playing 'none' sounds").setPhase(Phase.EARLY).setSide(Side.BOTH)
+            .addTargetedMod(TargetedMod.VANILLA).addMixinClasses("minecraft.shutup.MixinEntityLiving")
             .setApplyIf(() -> true)),
 
     MEMORY_FIXES_IC2(new MixinBuilder("Removes allocation spam from the Direction.applyTo method").setPhase(Phase.LATE)
