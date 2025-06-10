745d3ec1c8958d73e77346f05679743444bde104
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-06-10 18:50:09 +0200

    add mixins to synchonize from server to client the thrower of an item entity

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/packets/MixinS0EPacketSpawnObject_ItemThrower.java
@@ -0,0 +1,46 @@
+package com.mitchej123.hodgepodge.mixins.early.minecraft.packets;
+
+import java.io.IOException;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.play.server.S0EPacketSpawnObject;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.mitchej123.hodgepodge.mixins.interfaces.S0EPacketSpawnObjectExt;
+
+@Mixin(S0EPacketSpawnObject.class)
+public class MixinS0EPacketSpawnObject_ItemThrower implements S0EPacketSpawnObjectExt {
+
+    @Unique
+    private String hodgepodge$itemThrower = null;
+
+    @Inject(method = "<init>(Lnet/minecraft/entity/Entity;II)V", at = @At("TAIL"))
+    private void hodgepodge$init(Entity entity, int p_i45166_2_, int p_i45166_3_, CallbackInfo ci) {
+        if (entity instanceof EntityItem entityItem && entityItem.func_145800_j() != null) {
+            hodgepodge$itemThrower = entityItem.func_145800_j();
+        }
+    }
+
+    @Inject(method = "writePacketData", at = @At("TAIL"))
+    private void hodgepodge$write(PacketBuffer data, CallbackInfo ci) throws IOException {
+        data.writeStringToBuffer(hodgepodge$itemThrower == null ? "" : hodgepodge$itemThrower);
+    }
+
+    @Inject(method = "readPacketData", at = @At("TAIL"))
+    private void hodgepodge$read(PacketBuffer data, CallbackInfo ci) throws IOException {
+        final String name = data.readStringFromBuffer(16);
+        hodgepodge$itemThrower = name.isEmpty() ? null : name;
+    }
+
+    @Override
+    public String hodgepodge$getThrower() {
+        return hodgepodge$itemThrower;
+    }
+}
---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/interfaces/S0EPacketSpawnObjectExt.java
@@ -0,0 +1,6 @@
+package com.mitchej123.hodgepodge.mixins.interfaces;
+
+public interface S0EPacketSpawnObjectExt {
+
+    String hodgepodge$getThrower();
+}
