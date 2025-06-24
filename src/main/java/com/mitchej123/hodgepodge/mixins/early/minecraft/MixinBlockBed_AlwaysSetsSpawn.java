d83c909a7f375bac68e3bfef11fc6f8e9a156cc5
Caedis <Caedis@users.noreply.github.com>
2025-06-24 13:56:00 -0500

    Rework Bed Set Spawn Mixin (#566)

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinBlockBed_AlwaysSetsSpawn.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinBlockBed_AlwaysSetsSpawn.java
@@ -3,33 +3,37 @@ package com.mitchej123.hodgepodge.mixins.early.minecraft;
 import net.minecraft.block.BlockBed;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.ChunkCoordinates;
-import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 
 @Mixin(BlockBed.class)
 public class MixinBlockBed_AlwaysSetsSpawn {
 
-    @Inject(
+    @WrapOperation(
             method = "onBlockActivated",
             at = @At(
-                    value = "INVOKE_ASSIGN",
-                    target = "Lnet/minecraft/entity/player/EntityPlayer;sleepInBedAt(III)Lnet/minecraft/entity/player/EntityPlayer$EnumStatus;",
-                    shift = At.Shift.AFTER))
-    public void hodgepodge$setSpawn(World worldIn, int x, int y, int z, EntityPlayer player, int side, float subX,
-            float subY, float subZ, CallbackInfoReturnable<Boolean> cir) {
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/player/EntityPlayer;sleepInBedAt(III)Lnet/minecraft/entity/player/EntityPlayer$EnumStatus;"))
+    public EntityPlayer.EnumStatus hodgepodge$setSpawn(EntityPlayer instance, int x, int y, int z,
+            Operation<EntityPlayer.EnumStatus> original, @Local(argsOnly = true) World worldIn) {
         // do fake player activators dream of fake sheep?
-        if (!(player instanceof EntityPlayerMP entityPlayerMP)) return;
-
-        entityPlayerMP.setSpawnChunk(new ChunkCoordinates(x, y, z), false);
-        // not render over hotbar due to other mixin
-        entityPlayerMP.addChatComponentMessage(
-                new ChatComponentText(StatCollector.translateToLocal("hodgepodge.bed_respawn.msg")));
+        if (instance instanceof EntityPlayerMP entityPlayerMP) {
+            ChunkCoordinates bedPos = entityPlayerMP.getBedLocation(worldIn.provider.dimensionId);
+            ChunkCoordinates newBedPos = new ChunkCoordinates(x, y, z);
+            if (worldIn.isDaytime() && !newBedPos.equals(bedPos)) {
+                entityPlayerMP.setSpawnChunk(newBedPos, false);
+                entityPlayerMP.addChatComponentMessage(new ChatComponentTranslation("hodgepodge.bed_respawn.msg"));
+                return EntityPlayer.EnumStatus.OTHER_PROBLEM;
+            }
+        }
+        return original.call(instance, x, y, z);
     }
 }
