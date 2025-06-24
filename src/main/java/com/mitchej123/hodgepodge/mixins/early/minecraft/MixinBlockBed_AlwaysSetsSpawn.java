402c76a9e6a3b09c5b986188b6c61b56a723b8a1
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-06-24 20:48:55 +0200

    use chat message to match modern MC

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinBlockBed_AlwaysSetsSpawn.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinBlockBed_AlwaysSetsSpawn.java
@@ -3,17 +3,13 @@ package com.mitchej123.hodgepodge.mixins.early.minecraft;
 import net.minecraft.block.BlockBed;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.ChatStyle;
+import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.ChunkCoordinates;
-import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-import com.gtnewhorizon.gtnhlib.GTNHLib;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
@@ -29,26 +25,15 @@ public class MixinBlockBed_AlwaysSetsSpawn {
     public EntityPlayer.EnumStatus hodgepodge$setSpawn(EntityPlayer instance, int x, int y, int z,
             Operation<EntityPlayer.EnumStatus> original, @Local(argsOnly = true) World worldIn) {
         // do fake player activators dream of fake sheep?
-        if (!(instance instanceof EntityPlayerMP entityPlayerMP)) return original.call(instance, x, y, z);
-
-        ChunkCoordinates bedPos = entityPlayerMP.getBedLocation(worldIn.provider.dimensionId);
-
-        ChunkCoordinates newBedPos = new ChunkCoordinates(x, y, z);
-
-        if (worldIn.isDaytime() && !newBedPos.equals(bedPos)) {
-            entityPlayerMP.setSpawnChunk(newBedPos, false);
-
-            // render over hotbar since the caller doesn't handle OTHER_PROBLEM
-            GTNHLib.proxy.sendMessageAboveHotbar(
-                    entityPlayerMP,
-                    new ChatComponentText(StatCollector.translateToLocal("hodgepodge.bed_respawn.msg"))
-                            .setChatStyle(new ChatStyle().setColor(EnumChatFormatting.WHITE)),
-                    60,
-                    true,
-                    true);
-            return EntityPlayer.EnumStatus.OTHER_PROBLEM;
+        if (instance instanceof EntityPlayerMP entityPlayerMP) {
+            ChunkCoordinates bedPos = entityPlayerMP.getBedLocation(worldIn.provider.dimensionId);
+            ChunkCoordinates newBedPos = new ChunkCoordinates(x, y, z);
+            if (worldIn.isDaytime() && !newBedPos.equals(bedPos)) {
+                entityPlayerMP.setSpawnChunk(newBedPos, false);
+                entityPlayerMP.addChatComponentMessage(new ChatComponentTranslation("hodgepodge.bed_respawn.msg"));
+                return EntityPlayer.EnumStatus.OTHER_PROBLEM;
+            }
         }
-
         return original.call(instance, x, y, z);
     }
 }
