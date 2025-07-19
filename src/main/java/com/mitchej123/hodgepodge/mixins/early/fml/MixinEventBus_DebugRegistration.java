2603dfa4e729db5db5d20bca232669ef9d352b5d
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-07-19 02:45:36 +0200

    add debug flag to log time to register forge event handlers (#585)

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/fml/MixinEventBus_DebugRegistration.java
@@ -0,0 +1,71 @@
+package com.mitchej123.hodgepodge.mixins.early.fml;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.mitchej123.hodgepodge.util.FileLogger;
+
+import cpw.mods.fml.common.eventhandler.EventBus;
+
+@Mixin(value = EventBus.class, remap = false)
+public class MixinEventBus_DebugRegistration {
+
+    @Unique
+    private static final FileLogger debug$log = new FileLogger("EventRegistrationTime.txt");
+    @Unique
+    private static long debug$totalTime;
+
+    @Unique
+    private int debug$countMethods;
+    @Unique
+    private int debug$countHandlers;
+    @Unique
+    private long debug$timeStart;
+
+    @Inject(method = "register(Ljava/lang/Object;)V", at = @At("HEAD"))
+    private void debug$atHead(Object target, CallbackInfo ci) {
+        debug$countMethods = 0;
+        debug$countHandlers = 0;
+        debug$timeStart = System.nanoTime();
+    }
+
+    @Inject(
+            method = "register(Ljava/lang/Object;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/lang/Class;getDeclaredMethod(Ljava/lang/String;[Ljava/lang/Class;)Ljava/lang/reflect/Method;",
+                    shift = At.Shift.BEFORE))
+    private void debug$CountMethods(Object target, CallbackInfo ci) {
+        debug$countMethods++;
+    }
+
+    @Inject(
+            method = "register(Ljava/lang/Object;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcpw/mods/fml/common/eventhandler/EventBus;register(Ljava/lang/Class;Ljava/lang/Object;Ljava/lang/reflect/Method;Lcpw/mods/fml/common/ModContainer;)V",
+                    shift = At.Shift.BEFORE))
+    private void debug$CountHandlers(Object target, CallbackInfo ci) {
+        debug$countHandlers++;
+    }
+
+    @Inject(method = "register(Ljava/lang/Object;)V", at = @At(value = "TAIL"))
+    private void debug$atBottom(Object target, CallbackInfo ci) {
+        final long timeElapsed = System.nanoTime() - debug$timeStart;
+        synchronized (debug$log) {
+            debug$totalTime += timeElapsed;
+            debug$log.log("Registered event class : " + target.getClass().getName());
+            debug$log.log("Methods Visited : " + debug$countMethods);
+            debug$log.log("Handlers found : " + debug$countHandlers);
+            debug$log.log("Time elapsed : " + (timeElapsed / 1_000_000) + "ms");
+            debug$log.log("Total Time elapsed : " + (debug$totalTime / 1_000_000) + "ms");
+            if (debug$countHandlers == 0) {
+                debug$log.log("No handlers have been found in the registered class!!!");
+                debug$log.printStackTrace();
+            }
+        }
+    }
+}
--- src/main/java/com/mitchej123/hodgepodge/mixins/preinit/MixinLoadController_logModTimes.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/preinit/MixinLoadController_logModTimes.java
@@ -1,21 +1,18 @@
 package com.mitchej123.hodgepodge.mixins.preinit;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Map;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.mitchej123.hodgepodge.Common;
 import com.mitchej123.hodgepodge.mixins.hooks.ModProfileResult;
+import com.mitchej123.hodgepodge.util.FileLogger;
 
 import cpw.mods.fml.common.LoadController;
 import cpw.mods.fml.common.LoaderState;
@@ -26,7 +23,8 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 @Mixin(value = LoadController.class, remap = false)
 public class MixinLoadController_logModTimes {
 
-    private static Map<String, ArrayList<ModProfileResult>> results = new Object2ObjectOpenHashMap<>();
+    @Unique
+    private static Map<String, ArrayList<ModProfileResult>> hodgepodge$results = new Object2ObjectOpenHashMap<>();
 
     @WrapOperation(
             method = "propogateStateMessage",
@@ -39,10 +37,10 @@ public class MixinLoadController_logModTimes {
         final long start = System.nanoTime();
 
         original.call(instance, event, modContainer);
-        if (results == null) return;
+        if (hodgepodge$results == null) return;
 
         final long timeTaken = System.nanoTime() - start;
-        results.computeIfAbsent(event.getEventType(), k -> new ArrayList<>())
+        hodgepodge$results.computeIfAbsent(event.getEventType(), k -> new ArrayList<>())
                 .add(new ModProfileResult(timeTaken, modContainer));
     }
 
@@ -50,24 +48,18 @@ public class MixinLoadController_logModTimes {
             method = "Lcpw/mods/fml/common/LoadController;distributeStateMessage(Lcpw/mods/fml/common/LoaderState;[Ljava/lang/Object;)V",
             at = @At(value = "TAIL"))
     public void printResults(LoaderState state, Object[] eventData, CallbackInfo ci) {
-        if (results == null || state != LoaderState.AVAILABLE) return;
-        try {
-            PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("modtimes.csv", false)));
-
-            writer.println("event;modid;modname;time");
-            results.forEach((type, results) -> {
+        if (hodgepodge$results == null || state != LoaderState.AVAILABLE) return;
+        try (FileLogger logger = new FileLogger("modtimes.csv")) {
+            logger.log("event;modid;modname;time");
+            hodgepodge$results.forEach((type, results) -> {
                 results.sort(null);
                 results.forEach(result -> {
                     final String modid = result.mod.getModId();
-                    writer.println(type + ";" + modid + ";" + result.mod.getName() + ";" + result.time / 1000000);
+                    logger.log(type + ";" + modid + ";" + result.mod.getName() + ";" + result.time / 1000000);
                 });
-
             });
-            writer.close();
-        } catch (IOException e) {
-            Common.log.error("Failed to write modtimes.csv", e);
         }
-        results = null;
+        hodgepodge$results = null;
     }
 
 }
