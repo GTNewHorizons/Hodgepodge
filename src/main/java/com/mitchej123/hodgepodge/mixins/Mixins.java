715dbed5cff233bebdd6ede52bc079b06ab8d398
glowredman <35727266+glowredman@users.noreply.github.com>
2025-08-10 22:25:08 +0200

    Fix mistake made during merging of master

--- src/main/java/com/mitchej123/hodgepodge/mixins/Mixins.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/Mixins.java
@@ -722,7 +722,8 @@ public enum Mixins implements IMixins {
     DEBUG_EVENT_REGISTRATION(new MixinBuilder()
             .addCommonMixins("fml.MixinEventBus_DebugRegistration")
             .setApplyIf(() -> Boolean.getBoolean("hodgepodge.logEventTimes"))
-            .addCommonMixins("fml.MixinEventBus_DebugRegistration")),
+            .addCommonMixins("fml.MixinEventBus_DebugRegistration")
+            .setPhase(Phase.EARLY)),
     FIX_HOUSE_CHAR_RENDERING(new MixinBuilder()
             .addClientMixins("minecraft.MixinFontRenderer_House")
             .setApplyIf(() -> FixesConfig.fixHouseCharRendering)
