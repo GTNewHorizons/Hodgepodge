b19f33fa8852ccbe29549de388d038dd57cf98ab
Pilzinsel64 <pilzinsel64@gmx.de>
2025-08-10 13:57:03 +0200

    improve thaumcraft check for ee3

--- src/main/java/com/mitchej123/hodgepodge/mixins/late/thaumcraft/MixinUtils.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/thaumcraft/MixinUtils.java
@@ -3,19 +3,37 @@ package com.mitchej123.hodgepodge.mixins.late.thaumcraft;
 import net.minecraft.item.Item;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
 
 import cpw.mods.fml.common.Loader;
 
 @Mixin(thaumcraft.common.lib.utils.Utils.class)
 public class MixinUtils {
 
-    @Inject(method = "isEETransmutionItem", at = @At("HEAD"), remap = false, cancellable = true)
-    private static void hodgepodge$checkEEModExist(Item item, CallbackInfoReturnable<Boolean> ci) {
-        if (!Loader.isModLoaded("ee")) {
-            ci.cancel();
+    @Unique
+    private static boolean hp$initEECheck;
+
+    @Unique
+    @SuppressWarnings("rawtypes")
+    private static Class hp$classEE;
+
+    @Overwrite(remap = false)
+    @SuppressWarnings("unchecked")
+    public static boolean isEETransmutionItem(Item item) {
+        if (!hp$initEECheck) {
+            if (Loader.isModLoaded("ee")) {
+                try {
+                    hp$classEE = Class.forName("com.pahimar.ee3.item.ITransmutationStone");
+                } catch (Exception var3) {
+                    ;
+                }
+            }
+            hp$initEECheck = true;
         }
+        if (hp$classEE != null && hp$classEE.isAssignableFrom(item.getClass())) {
+            return true;
+        }
+        return false;
     }
 }
