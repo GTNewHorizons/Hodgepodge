79b9c892a8fa10dd7d811fa6a3e8de870f3659a2
Fabian Maurer <dark.shadow4@web.de>
2025-09-26 00:38:00 +0200

    Logging for cross thread nbt accesses

--- src/main/java/com/mitchej123/hodgepodge/LoggingNBTTagMap.java
+++ src/main/java/com/mitchej123/hodgepodge/LoggingNBTTagMap.java
@@ -1,5 +1,7 @@
 package com.mitchej123.hodgepodge;
 
+import java.util.HashSet;
+
 import cpw.mods.fml.common.FMLLog;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 
@@ -7,34 +9,34 @@ public class LoggingNBTTagMap<K, V> extends Object2ObjectOpenHashMap<K, V> {
 
     Thread thread;
 
+    private static final HashSet<Object> warned = new HashSet<>();
+
     public LoggingNBTTagMap() {
         thread = Thread.currentThread();
     }
 
+    private void check(Object k) {
+        if (thread != Thread.currentThread()) {
+            synchronized (warned) {
+                if (!warned.contains(k)) {
+                    warned.add(k);
+                    FMLLog.warning("Cross thread access in NBTTagCompound!");
+                    Thread.dumpStack();
+                }
+            }
+
+        }
+    }
+
     @Override
     public V put(final K k, final V v) {
-        if (thread != Thread.currentThread()) {
-            FMLLog.warning("Cross thread access in NBTTagCompound!");
-            Thread.dumpStack();
-        }
+        check(k);
         return super.put(k, v);
     }
 
     @Override
     public V remove(Object k) {
-        if (thread != Thread.currentThread()) {
-            FMLLog.warning("Cross thread access in NBTTagCompound!");
-            Thread.dumpStack();
-        }
+        check(k);
         return super.remove(k);
-    }
-
-    @Override
-    public void clear() {
-        if (thread != Thread.currentThread()) {
-            FMLLog.warning("Cross thread access in NBTTagCompound!");
-            Thread.dumpStack();
-        }
-        super.clear();
     }
 }
