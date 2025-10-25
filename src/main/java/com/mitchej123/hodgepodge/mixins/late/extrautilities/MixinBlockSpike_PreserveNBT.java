82ef10fa1c807cb1c7ab806a1fe45d5eeee1bbf2
Kynake Risk <kynakerisk@gmail.com>
2025-10-24 23:59:34 -0300

    Replace @Local with args in mixin method

--- src/main/java/com/mitchej123/hodgepodge/mixins/late/extrautilities/MixinBlockSpike_PreserveNBT.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/extrautilities/MixinBlockSpike_PreserveNBT.java
@@ -1,14 +1,15 @@
 package com.mitchej123.hodgepodge.mixins.late.extrautilities;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
+import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import com.rwtema.extrautils.block.BlockSpike;
 
 @Mixin(BlockSpike.class)
@@ -20,8 +21,8 @@ public abstract class MixinBlockSpike_PreserveNBT {
             at = @At(
                     value = "INVOKE",
                     target = "Lcom/rwtema/extrautils/tileentity/TileEntityEnchantedSpike;setEnchantmentTagList(Lnet/minecraft/nbt/NBTTagList;)V"))
-    private NBTTagList hodgepodge$injectNBTCompoundIntoTagList(NBTTagList enchantments,
-            @Local(name = "stack") ItemStack stack) {
+    private NBTTagList hodgepodge$injectNBTCompoundIntoTagList(NBTTagList enchantments, World world, int x, int y,
+            int z, EntityLivingBase entity, ItemStack stack) {
         if (enchantments == null || enchantments.tagCount() == 0
                 || stack.stackTagCompound == null
                 || stack.stackTagCompound.hasNoTags()) {
