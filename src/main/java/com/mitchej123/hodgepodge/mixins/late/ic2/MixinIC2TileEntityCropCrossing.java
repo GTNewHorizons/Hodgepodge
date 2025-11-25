d60a678b10fd6523474a33a409dc51030f0513b0
SuperSoupr <26164930+SuperSoupr@users.noreply.github.com>
2025-11-25 21:26:10 +0200

    Implement dev vote results

--- src/main/java/com/mitchej123/hodgepodge/mixins/late/ic2/MixinIC2TileEntityCropCrossing.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/ic2/MixinIC2TileEntityCropCrossing.java
@@ -63,7 +63,7 @@ public class MixinIC2TileEntityCropCrossing extends TileEntity {
     public void hodgepodge$attemptCrossing(CallbackInfoReturnable<Boolean> cir) {
         TileEntityCrop thisObject = (TileEntityCrop) (Object) this;
         if (thisObject.statGrowth >= 24) {
-            if (IC2.random.nextInt(2) == 1) thisObject.statGrowth = 23;
+            if (IC2.random.nextInt(4) == 0) thisObject.statGrowth = 23;
         }
     }
 
@@ -75,9 +75,17 @@ public class MixinIC2TileEntityCropCrossing extends TileEntity {
                     from = @At(value = "INVOKE", target = "Ljava/util/List;size()I", ordinal = 1),
                     to = @At(value = "INVOKE", target = "Lic2/core/util/Util;limit(III)I")),
             remap = false)
-    public int hodgepodge$attemptCrossing(Random instance, int i) {
+    public int hodgepodge$attemptCrossing(Random instance, int i, @Local List<TileEntityCrop> cropTes) {
         // Resulting line will look like
-        // this.statGrowth += IC2.random.nextInt(2 * count) - (count - 1);
-        return IC2.random.nextInt(i - 1) + 1;
+        // this.statGrowth += IC2.random.nextInt(count * 1.5) - (count / 2);
+        int count = cropTes.size();
+        double x = count * 0.5F;
+        int y;
+        if (x % 1 != 0)
+            // if there are 3 parents randomly choose between (-1 to 3) and (-2 to 3)
+            y = (int) (Math.floor(x) + IC2.random.nextInt(2));
+        else
+            y = (int) x;
+        return IC2.random.nextInt(count + y + 1) - y + count;
     }
 }
