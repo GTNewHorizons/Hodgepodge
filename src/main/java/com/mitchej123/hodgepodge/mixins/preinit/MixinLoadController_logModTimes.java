eb7154b500e245f0ecf82411d2847bf7df60e05c
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-12-03 21:36:45 +0100

    write mod times results in a shutdown hook in order to capture the server events too

--- src/main/java/com/mitchej123/hodgepodge/mixins/preinit/MixinLoadController_logModTimes.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/preinit/MixinLoadController_logModTimes.java
@@ -6,8 +6,6 @@ import java.util.Map;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
@@ -15,7 +13,6 @@ import com.mitchej123.hodgepodge.core.shared.FileLogger;
 import com.mitchej123.hodgepodge.mixins.hooks.ModProfileResult;
 
 import cpw.mods.fml.common.LoadController;
-import cpw.mods.fml.common.LoaderState;
 import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.event.FMLEvent;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
@@ -24,7 +21,7 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 public class MixinLoadController_logModTimes {
 
     @Unique
-    private static Map<String, ArrayList<ModProfileResult>> hodgepodge$results = new Object2ObjectOpenHashMap<>();
+    private static final Map<String, ArrayList<ModProfileResult>> hodgepodge$results = new Object2ObjectOpenHashMap<>();
 
     @WrapOperation(
             method = "propogateStateMessage",
@@ -35,31 +32,26 @@ public class MixinLoadController_logModTimes {
             Operation<Void> original) {
 
         final long start = System.nanoTime();
-
         original.call(instance, event, modContainer);
-        if (hodgepodge$results == null) return;
-
         final long timeTaken = System.nanoTime() - start;
+
         hodgepodge$results.computeIfAbsent(event.getEventType(), k -> new ArrayList<>())
                 .add(new ModProfileResult(timeTaken, modContainer));
     }
 
-    @Inject(
-            method = "Lcpw/mods/fml/common/LoadController;distributeStateMessage(Lcpw/mods/fml/common/LoaderState;[Ljava/lang/Object;)V",
-            at = @At(value = "TAIL"))
-    public void printResults(LoaderState state, Object[] eventData, CallbackInfo ci) {
-        if (hodgepodge$results == null || state != LoaderState.AVAILABLE) return;
-        try (FileLogger logger = new FileLogger("modtimes.csv")) {
-            logger.log("event;modid;modname;time");
-            hodgepodge$results.forEach((type, results) -> {
-                results.sort(null);
-                results.forEach(result -> {
-                    final String modid = result.mod.getModId();
-                    logger.log(type + ";" + modid + ";" + result.mod.getName() + ";" + result.time / 1_000_000);
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (FileLogger logger = new FileLogger("modtimes.csv")) {
+                logger.log("event;modid;modname;time");
+                hodgepodge$results.forEach((type, results) -> {
+                    results.sort(null);
+                    results.forEach(result -> {
+                        final String modid = result.mod.getModId();
+                        logger.log(type + ";" + modid + ";" + result.mod.getName() + ";" + result.time / 1_000_000);
+                    });
                 });
-            });
-        }
-        hodgepodge$results = null;
+            }
+        }));
     }
 
 }
