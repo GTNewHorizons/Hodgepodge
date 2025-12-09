cf1bbb56d9b2ace8ef0580864576ff741a650a58
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-12-09 22:59:03 +0100

    print results on startup and on shutdown

--- src/main/java/com/mitchej123/hodgepodge/mixins/preinit/MixinLoadController_logModTimes.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/preinit/MixinLoadController_logModTimes.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
@@ -13,6 +15,7 @@ import com.mitchej123.hodgepodge.core.shared.FileLogger;
 import com.mitchej123.hodgepodge.mixins.hooks.ModProfileResult;
 
 import cpw.mods.fml.common.LoadController;
+import cpw.mods.fml.common.LoaderState;
 import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.event.FMLEvent;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
@@ -22,6 +25,10 @@ public class MixinLoadController_logModTimes {
 
     @Unique
     private static final Map<String, ArrayList<ModProfileResult>> hodgepodge$results = new Object2ObjectOpenHashMap<>();
+
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> hp$printResults("modtimes_shutdown.csv")));
+    }
 
     @WrapOperation(
             method = "propogateStateMessage",
@@ -39,19 +46,26 @@ public class MixinLoadController_logModTimes {
                 .add(new ModProfileResult(timeTaken, modContainer));
     }
 
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileLogger logger = new FileLogger("modtimes.csv")) {
-                logger.log("event;modid;modname;time");
-                hodgepodge$results.forEach((type, results) -> {
-                    results.sort(null);
-                    results.forEach(result -> {
-                        final String modid = result.mod.getModId();
-                        logger.log(type + ";" + modid + ";" + result.mod.getName() + ";" + result.time / 1_000_000);
-                    });
+    @Inject(
+            method = "distributeStateMessage(Lcpw/mods/fml/common/LoaderState;[Ljava/lang/Object;)V",
+            at = @At(value = "TAIL"))
+    private void printResults(LoaderState state, Object[] eventData, CallbackInfo ci) {
+        if (state != LoaderState.AVAILABLE) return;
+        hp$printResults("modtimes_startup.csv");
+    }
+
+    @Unique
+    private static void hp$printResults(String filename) {
+        try (FileLogger logger = new FileLogger(filename)) {
+            logger.log("event;modid;modname;time");
+            hodgepodge$results.forEach((type, results) -> {
+                results.sort(null);
+                results.forEach(result -> {
+                    final String modid = result.mod.getModId();
+                    logger.log(type + ";" + modid + ";" + result.mod.getName() + ";" + result.time / 1_000_000);
                 });
-            }
-        }));
+            });
+        }
     }
 
 }
