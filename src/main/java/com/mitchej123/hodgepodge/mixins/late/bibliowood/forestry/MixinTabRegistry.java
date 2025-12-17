65e5e4f6045961097a428192e73a7d7714e97184
DrParadox7 <gustavo.caliari@live.com>
2025-12-17 12:17:59 +0100

    Improve readability
    Apply Spotless

--- src/main/java/com/mitchej123/hodgepodge/mixins/late/bibliowood/forestry/MixinTabRegistry.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/bibliowood/forestry/MixinTabRegistry.java
@@ -3,6 +3,7 @@ package com.mitchej123.hodgepodge.mixins.late.bibliowood.forestry;
 import net.minecraft.block.Block;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
@@ -12,16 +13,74 @@ import jds.bibliowood.forestrywood.WoodsLoader;
 @Mixin(WoodsLoader.class)
 public class MixinTabRegistry {
 
-    @ModifyVariable(method = "initRecipes", at = @At(value = "STORE", ordinal = 0), remap = false)
+    @Unique
+    private static final int hodgepodge$acaciaMeta = 2;
+    @Unique
+    private static final int hodgepodge$balsaMeta = 11;
+    @Unique
+    private static final int hodgepodge$baobabMeta = 6;
+    @Unique
+    private static final int hodgepodge$cherryMeta = 15;
+    @Unique
+    private static final int hodgepodge$chestnutMeta = 4;
+    @Unique
+    private static final int hodgepodge$citrusMeta = 23;
+    @Unique
+    private static final int hodgepodge$ebonyMeta = 9;
+    @Unique
+    private static final int hodgepodge$greenheartMeta = 14;
+    @Unique
+    private static final int hodgepodge$kapokMeta = 8;
+    @Unique
+    private static final int hodgepodge$larchMeta = 0;
+    @Unique
+    private static final int hodgepodge$limeMeta = 3;
+    @Unique
+    private static final int hodgepodge$mahoeMeta = 16;
+    @Unique
+    private static final int hodgepodge$mahogamyMeta = 10;
+    @Unique
+    private static final int hodgepodge$mapleMeta = 22;
+    @Unique
+    private static final int hodgepodge$palmMeta = 18;
+    @Unique
+    private static final int hodgepodge$papayaMeta = 19;
+    @Unique
+    private static final int hodgepodge$pineMeta = 20;
+    @Unique
+    private static final int hodgepodge$plumMeta = 21;
+    @Unique
+    private static final int hodgepodge$poplarMeta = 17;
+    @Unique
+    private static final int hodgepodge$sequoiaMeta = 7;
+    @Unique
+    private static final int hodgepodge$teakMeta = 1;
+    @Unique
+    private static final int hodgepodge$walnutMeta = 13;
+    @Unique
+    private static final int hodgepodge$wengeMeta = 5;
+    @Unique
+    private static final int hodgepodge$willowMeta = 12;
+
+    @Unique
+    private static final int[] hodgepodge$correctedMetas = new int[] { hodgepodge$acaciaMeta, hodgepodge$balsaMeta,
+            hodgepodge$baobabMeta, hodgepodge$cherryMeta, hodgepodge$chestnutMeta, hodgepodge$citrusMeta,
+            hodgepodge$ebonyMeta, hodgepodge$greenheartMeta, hodgepodge$kapokMeta, hodgepodge$larchMeta,
+            hodgepodge$limeMeta, hodgepodge$mahoeMeta, hodgepodge$mahogamyMeta, hodgepodge$mapleMeta,
+            hodgepodge$palmMeta, hodgepodge$papayaMeta, hodgepodge$pineMeta, hodgepodge$plumMeta, hodgepodge$poplarMeta,
+            hodgepodge$sequoiaMeta, hodgepodge$teakMeta, hodgepodge$walnutMeta, hodgepodge$wengeMeta,
+            hodgepodge$willowMeta };
+
+    @ModifyVariable(method = "initRecipes", at = @At(value = "STORE"), remap = false)
     private static Block[] hodgepodge$fixPlankID(Block[] metas) {
-        Block id = GameRegistry.findBlock("Forestry", "planks");
+        final Block id = GameRegistry.findBlock("Forestry", "planks");
         return new Block[] { id, id, id, id, id, id, id, id, id, id, id, id, id, id, id, id, id, id, id, id, id, id, id,
                 id };
     }
 
     @ModifyVariable(method = "initRecipes", at = @At(value = "STORE"), remap = false)
     private static int[] hodgepodge$fixPlankMeta(int[] metas) {
-        return new int[] { 2, 11, 6, 15, 4, 23, 9, 14, 8, 0, 3, 16, 10, 22, 18, 19, 20, 21, 17, 7, 1, 13, 5, 12 };
+        return hodgepodge$correctedMetas;
     }
 
     @ModifyVariable(method = "initRecipes", at = @At(value = "STORE"), ordinal = 1, remap = false)
@@ -33,6 +92,6 @@ public class MixinTabRegistry {
 
     @ModifyVariable(method = "initRecipes", at = @At(value = "STORE"), ordinal = 1, remap = false)
     private static int[] hodgepodge$fixSlabMeta(int[] metas) {
-        return new int[] { 2, 11, 6, 15, 4, 23, 9, 14, 8, 0, 3, 16, 10, 22, 18, 19, 20, 21, 17, 7, 1, 13, 5, 12 };
+        return hodgepodge$correctedMetas;
     }
 }
