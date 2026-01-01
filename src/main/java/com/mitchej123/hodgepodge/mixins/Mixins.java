e67fe82deaec669b2dc0c975f4e59da55267dcdf
Martin Robertz <dream-master@gmx.net>
2026-01-01 19:19:37 +0100

    fix merge conflict

--- src/main/java/com/mitchej123/hodgepodge/mixins/Mixins.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/Mixins.java
@@ -788,6 +788,7 @@ public enum Mixins implements IMixins {
     MAINTAIN_SLIME_HEALTH(new MixinBuilder("Prevent slimes from resetting to max health when loaded from NBT")
             .setApplyIf(() -> FixesConfig.maintainSlimeHealth)
             .addCommonMixins("minecraft.MixinEntitySlime_MaintainHealth")
+            .setPhase(Phase.EARLY)),
     EAT_FOOD_IN_CREATIVE(new MixinBuilder("Allow players to eat food in Creative")
             .addCommonMixins("minecraft.MixinEntityPlayer_EatInCreative", "minecraft.MixinItemFood_DontConsumeCreative")
             .setApplyIf(() -> TweaksConfig.allowEatingFoodInCreative)
