dacd65e14e2dde354c78be0e125a3026eaf13a9c
Jason Mitchell <mitchej+github@gmail.com>
2026-01-26 08:38:02 -0800

    More threadsafe (#727)

--- src/main/java/com/mitchej123/hodgepodge/util/FastUtilLongHashMap.java
+++ src/main/java/com/mitchej123/hodgepodge/util/FastUtilLongHashMap.java
@@ -1,6 +1,10 @@
 package com.mitchej123.hodgepodge.util;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import net.minecraft.util.LongHashMap;
 
@@ -9,38 +13,67 @@ import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 
 public class FastUtilLongHashMap extends LongHashMap {
 
-    private final Long2ObjectMap<Object> map;
-
-    public FastUtilLongHashMap() {
-        map = new Long2ObjectOpenHashMap<>();
-    }
+    private final Long2ObjectMap<Object> map = new Long2ObjectOpenHashMap<>();
+    private final ReadWriteLock rwLock = new ReentrantReadWriteLock();
+    private final Lock readLock = rwLock.readLock();
+    private final Lock writeLock = rwLock.writeLock();
 
     @Override
     public int getNumHashElements() {
-        return map.size();
+        readLock.lock();
+        try {
+            return map.size();
+        } finally {
+            readLock.unlock();
+        }
     }
 
     @Override
     public Object getValueByKey(long key) {
-        return map.get(key);
+        readLock.lock();
+        try {
+            return map.get(key);
+        } finally {
+            readLock.unlock();
+        }
     }
 
     @Override
     public boolean containsItem(long key) {
-        return map.containsKey(key);
+        readLock.lock();
+        try {
+            return map.containsKey(key);
+        } finally {
+            readLock.unlock();
+        }
     }
 
     @Override
     public void add(long key, Object value) {
-        map.put(key, value);
+        writeLock.lock();
+        try {
+            map.put(key, value);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     @Override
     public Object remove(long key) {
-        return map.remove(key);
+        writeLock.lock();
+        try {
+            return map.remove(key);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public Iterator<Object> valuesIterator() {
-        return map.values().iterator();
+        readLock.lock();
+        try {
+            return new ArrayList<>(map.values()).iterator();
+        } finally {
+            readLock.unlock();
+        }
     }
 }
