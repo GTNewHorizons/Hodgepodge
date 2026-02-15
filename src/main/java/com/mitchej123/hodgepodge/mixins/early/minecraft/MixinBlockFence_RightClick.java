043e2c9f51923d5a4dc768083f1f0b861b918e23
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2026-02-15 15:55:23 -0500

    change to inject

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinBlockFence_RightClick.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinBlockFence_RightClick.java
@@ -2,11 +2,12 @@ package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
 import net.minecraft.block.BlockFence;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemLead;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockFence.class)
 public class MixinBlockFence_RightClick {
@@ -15,12 +16,11 @@ public class MixinBlockFence_RightClick {
      * @author JB
      * @reason Fix fence always right-clicking
      */
-    @Overwrite
-    public boolean onBlockActivated(World worldIn, int x, int y, int z, EntityPlayer player, int side, float subX,
-            float subY, float subZ) {
-        if (!worldIn.isRemote) {
-            return ItemLead.func_150909_a(player, worldIn, x, y, z);
+    @Inject(method = "onBlockActivated", at = @At("HEAD"), cancellable = true)
+    private void onBlockActivated(World worldIn, int x, int y, int z, EntityPlayer player, int side, float subX,
+            float subY, float subZ, CallbackInfoReturnable<Boolean> cir) {
+        if (worldIn.isRemote) {
+            cir.setReturnValue(false);
         }
-        return false;
     }
 }
