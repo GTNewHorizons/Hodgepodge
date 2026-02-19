76a7f19c77dc13a2b50f7bda7df6807a57fb3901
Jason Mitchell <mitchej@gmail.com>
2026-02-19 14:17:25 -0800

    Inject instead of redirect :facepalm:

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinWorldGenLiquids_NoImmediateUpdates.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinWorldGenLiquids_NoImmediateUpdates.java
@@ -2,23 +2,24 @@ package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
 import java.util.Random;
 
-import net.minecraft.block.Block;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenLiquids;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(WorldGenLiquids.class)
 public class MixinWorldGenLiquids_NoImmediateUpdates {
 
-    @Redirect(
+    @Inject(
             method = "generate",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/block/Block;updateTick(Lnet/minecraft/world/World;IIILjava/util/Random;)V"))
-    private void hodgepodge$skipImmediateUpdate(Block block, World world, int x, int y, int z, Random random) {
+    private void hodgepodge$disableImmediateUpdates(World world, Random random, int x, int y, int z,
+            CallbackInfoReturnable<Boolean> cir) {
         world.scheduledUpdatesAreImmediate = false;
     }
 }
