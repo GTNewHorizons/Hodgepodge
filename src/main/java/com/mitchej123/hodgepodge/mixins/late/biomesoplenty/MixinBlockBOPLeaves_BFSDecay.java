39ff5c4571f355b47b113b14eb0fe900142a4f4d
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-02-23 15:00:54 +0100

    add BlockBOPAppleLeaves and BlockBOPPersimmonLeaves to BFS leaf decay mixins

--- src/main/java/com/mitchej123/hodgepodge/mixins/late/biomesoplenty/MixinBlockBOPLeaves_BFSDecay.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/biomesoplenty/MixinBlockBOPLeaves_BFSDecay.java
@@ -13,10 +13,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mitchej123.hodgepodge.mixins.hooks.LeafDecayHooks;
 
+import biomesoplenty.common.blocks.BlockBOPAppleLeaves;
 import biomesoplenty.common.blocks.BlockBOPColorizedLeaves;
 import biomesoplenty.common.blocks.BlockBOPLeaves;
+import biomesoplenty.common.blocks.BlockBOPPersimmonLeaves;
 
-@Mixin(value = { BlockBOPColorizedLeaves.class, BlockBOPLeaves.class })
+@Mixin(
+        value = { BlockBOPAppleLeaves.class, BlockBOPColorizedLeaves.class, BlockBOPLeaves.class,
+                BlockBOPPersimmonLeaves.class })
 public class MixinBlockBOPLeaves_BFSDecay {
 
     @Inject(
@@ -24,7 +28,7 @@ public class MixinBlockBOPLeaves_BFSDecay {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;checkChunksExist(IIIIII)Z"),
             cancellable = true)
     private void hodgepodge$bfsDecay(World world, int x, int y, int z, Random random, CallbackInfo ci,
-            @Local(name = "meta") int meta, @Local(name = "b0") byte b0) {
-        LeafDecayHooks.handleDecayChecked((Block) (Object) this, world, x, y, z, meta, b0, ci);
+            @Local(name = "meta") int meta, @Local(name = "b0") byte range) {
+        LeafDecayHooks.handleDecayChecked((Block) (Object) this, world, x, y, z, meta, range, ci);
     }
 }
