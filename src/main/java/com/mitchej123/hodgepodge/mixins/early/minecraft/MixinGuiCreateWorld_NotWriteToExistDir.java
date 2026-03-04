7cd4f3add468e3f9746f374b2c3f58e8859a2038
copi143 <copi143@outlook.com>
2026-03-04 21:29:16 +0800

    fix style

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinGuiCreateWorld_NotWriteToExistDir.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinGuiCreateWorld_NotWriteToExistDir.java
@@ -1,9 +1,12 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
+import java.io.File;
+
 import net.minecraft.client.gui.GuiCreateWorld;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.world.storage.ISaveFormat;
 import net.minecraft.world.storage.SaveFormatOld;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,10 +14,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.io.File;
-
 @Mixin(GuiCreateWorld.class)
 public abstract class MixinGuiCreateWorld_NotWriteToExistDir {
+
     @Unique
     private static String hodgepodge$handleConflict(ISaveFormat saveFormat, String worldName) {
         String dirName = worldName;
@@ -37,11 +39,10 @@ public abstract class MixinGuiCreateWorld_NotWriteToExistDir {
             method = "func_146317_a(Lnet/minecraft/world/storage/ISaveFormat;Ljava/lang/String;)Ljava/lang/String;",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/world/storage/ISaveFormat;getWorldInfo(Ljava/lang/String;)Lnet/minecraft/world/storage/WorldInfo;"
-            ),
-            cancellable = true
-    )
-    private static void hodgepodge$handleConflict1(ISaveFormat saveFormat, String worldName, CallbackInfoReturnable<String> cir) {
+                    target = "Lnet/minecraft/world/storage/ISaveFormat;getWorldInfo(Ljava/lang/String;)Lnet/minecraft/world/storage/WorldInfo;"),
+            cancellable = true)
+    private static void hodgepodge$handleConflict1(ISaveFormat saveFormat, String worldName,
+            CallbackInfoReturnable<String> cir) {
         cir.setReturnValue(hodgepodge$handleConflict(saveFormat, worldName));
     }
 
@@ -49,10 +50,8 @@ public abstract class MixinGuiCreateWorld_NotWriteToExistDir {
             method = "actionPerformed",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/Minecraft;launchIntegratedServer(Ljava/lang/String;Ljava/lang/String;Lnet/minecraft/world/WorldSettings;)V"
-            ),
-            index = 0
-    )
+                    target = "Lnet/minecraft/client/Minecraft;launchIntegratedServer(Ljava/lang/String;Ljava/lang/String;Lnet/minecraft/world/WorldSettings;)V"),
+            index = 0)
     private String hodgepodge$handleConflict2(String folderName) {
         return hodgepodge$handleConflict(((GuiScreen) (Object) this).mc.getSaveLoader(), folderName);
     }
