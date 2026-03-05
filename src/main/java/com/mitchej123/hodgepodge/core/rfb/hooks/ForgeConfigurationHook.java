e90bc8531614d46f571f38f98e99586596b1dff5
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-03-05 18:33:55 +0100

    Fix forge config memory bomb (#775)

--- src/main/java/com/mitchej123/hodgepodge/core/rfb/hooks/ForgeConfigurationHook.java
+++ src/main/java/com/mitchej123/hodgepodge/core/rfb/hooks/ForgeConfigurationHook.java
@@ -59,4 +59,16 @@ public class ForgeConfigurationHook {
         final TreeMap<String, V> sortedMap = new TreeMap<>(map);
         return sortedMap.values();
     }
+
+    public static String getDefaultValues(String[] values) {
+        final StringBuilder sb = new StringBuilder(32 * values.length);
+        for (int i = 0; i < values.length; i++) {
+            if (i == 0) {
+                sb.append("[").append(values[i]).append("]");
+            } else {
+                sb.append(", [").append(values[i]).append("]");
+            }
+        }
+        return sb.toString();
+    }
 }
