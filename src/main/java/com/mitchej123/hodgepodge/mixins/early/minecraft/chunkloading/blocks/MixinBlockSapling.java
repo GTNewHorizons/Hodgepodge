ce4d4febd38bd8be507e9e7c882f0b5b1f493b51
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-03-06 22:05:45 +0100

    fix BlockSapling

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/chunkloading/blocks/MixinBlockSapling.java
@@ -0,0 +1,32 @@
+package com.mitchej123.hodgepodge.mixins.early.minecraft.chunkloading.blocks;
+
+import java.util.Random;
+
+import net.minecraft.block.BlockSapling;
+import net.minecraft.world.World;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+// need our code to be injected before
+// squeek.applecore.mixins.early.minecraft.BlockSaplingMixin
+// so we set priority to run after
+@Mixin(value = BlockSapling.class, priority = 1100)
+public class MixinBlockSapling {
+
+    @Inject(
+            method = "updateTick",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/block/BlockBush;updateTick(Lnet/minecraft/world/World;IIILjava/util/Random;)V",
+                    shift = At.Shift.AFTER),
+            cancellable = true)
+    private void checkExist(World worldIn, int x, int y, int z, Random random, CallbackInfo ci) {
+        final int offset = 1;
+        if (!worldIn.checkChunksExist(x - offset, y, z - offset, x + offset, y, z + offset)) {
+            ci.cancel();
+        }
+    }
+}
