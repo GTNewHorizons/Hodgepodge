7b424ad90a34c43375ed04e69614885cc9278227
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-03-06 21:48:42 +0100

    change MixinBlockGrass to check only once if the chunk is loaded

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/chunkloading/blocks/MixinBlockGrass.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/chunkloading/blocks/MixinBlockGrass.java
@@ -8,48 +8,42 @@ import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(BlockGrass.class)
 public class MixinBlockGrass {
 
     /**
-     * @author tth05
-     * @reason Small performance improvements. Prevent chunk loading, avoid doing any work on blocks that can't turn to
-     *         grass, remove duplicate {@link World#getBlockLightValue(int, int, int)} call.
+     * @author tth05, Alexdoru
+     * @reason 1. Prevent loading unloading chunks when calling getBlock 2. Cache identical block calls 3. Remove unused
+     *         getBlock call 4. Give more chance for grass to spread if it randomly selects this block 5. Skip useless
+     *         getBlock calls
      */
     @Overwrite
     public void updateTick(World worldIn, int x, int y, int z, Random random) {
         if (worldIn.isRemote) return;
 
-        if (!hodgepodge$canGetBlockLightSafely(worldIn, x, z)) {
+        final int offset = 2;
+        if (!worldIn.checkChunksExist(x - offset, 0, z - offset, x + offset, 0, z + offset)) {
             return;
         }
-        int blockLightValue = worldIn.getBlockLightValue(x, y + 1, z);
-        if (blockLightValue < 4 && worldIn.getBlockLightOpacity(x, y + 1, z) > 2) {
+        final int lightValue = worldIn.getBlockLightValue(x, y + 1, z);
+        if (lightValue < 4 && worldIn.getBlockLightOpacity(x, y + 1, z) > 2) {
             worldIn.setBlock(x, y, z, Blocks.dirt);
-        } else if (blockLightValue >= 9) {
+        } else if (lightValue >= 9) {
             for (int i = 0; i < 4; ++i) {
-                final int targetX = x + random.nextInt(3) - 1;
-                final int targetY = y + random.nextInt(5) - 3;
-                final int targetZ = z + random.nextInt(3) - 1;
-
-                if (targetX == x && targetZ == z && (targetY == y || targetY == y - 1)) continue;
-                if (!worldIn.blockExists(targetX, targetY, targetZ)) continue;
-
-                if (worldIn.getBlock(targetX, targetY, targetZ) == Blocks.dirt
-                        && worldIn.getBlockMetadata(targetX, targetY, targetZ) == 0
-                        && worldIn.getBlockLightOpacity(targetX, targetY + 1, targetZ) <= 2
-                        && hodgepodge$canGetBlockLightSafely(worldIn, targetX, targetZ)
-                        && worldIn.getBlockLightValue(targetX, targetY + 1, targetZ) >= 4) {
-                    worldIn.setBlock(targetX, targetY, targetZ, Blocks.grass);
+                final int newX = x + random.nextInt(3) - 1;
+                final int newY = y + random.nextInt(5) - 3;
+                final int newZ = z + random.nextInt(3) - 1;
+                if (newX == x && newZ == z && (newY == y || newY == y - 1)) {
+                    i--; // give another chance for grass to spread
+                    continue;
+                }
+                if (worldIn.getBlock(newX, newY, newZ) == Blocks.dirt && worldIn.getBlockMetadata(newX, newY, newZ) == 0
+                        && worldIn.getBlockLightOpacity(newX, newY + 1, newZ) <= 2
+                        && worldIn.getBlockLightValue(newX, newY + 1, newZ) >= 4) {
+                    worldIn.setBlock(newX, newY, newZ, Blocks.grass);
                 }
             }
         }
-    }
-
-    @Unique
-    private static boolean hodgepodge$canGetBlockLightSafely(World worldIn, int x, int z) {
-        return worldIn.checkChunksExist(x - 1, 0, z - 1, x + 1, 0, z + 1);
     }
 }
