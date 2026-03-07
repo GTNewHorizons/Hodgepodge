dd053592708c16b36b1d840728dbfd4481e124b9
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-03-07 01:16:03 +0100

    actually be before apple core

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/chunkloading/blocks/MixinBlockMushroom.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/chunkloading/blocks/MixinBlockMushroom.java
@@ -1,24 +1,23 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft.chunkloading.blocks;
 
+import java.util.Random;
+
 import net.minecraft.block.BlockMushroom;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import com.llamalad7.mixinextras.sugar.Cancellable;
 
 @Mixin(BlockMushroom.class)
 public class MixinBlockMushroom {
 
-    @ModifyVariable(method = "updateTick", at = @At(value = "STORE", ordinal = 0))
-    private byte checkExist(byte b0, World worldIn, int x, int y, int z, @Cancellable CallbackInfo ci) {
+    @Inject(method = "updateTick", at = @At("HEAD"), cancellable = true)
+    private void checkExist(World worldIn, int x, int y, int z, Random random, CallbackInfo ci) {
         final int offset = 5;
         if (!worldIn.checkChunksExist(x - offset, y, z - offset, x + offset, y, z + offset)) {
             ci.cancel();
         }
-        return b0;
     }
 }
