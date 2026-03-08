d33ffe2ba98b46192411fff5414bdf8b11b6b2d8
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-03-08 11:56:34 +0100

    fix mixin compat with aquaacrobatics mod

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/chunkloading/blocks/MixinBlockMycelium.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/chunkloading/blocks/MixinBlockMycelium.java
@@ -9,7 +9,7 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
-@Mixin(BlockMycelium.class)
+@Mixin(value = BlockMycelium.class, priority = 800)
 public class MixinBlockMycelium {
 
     /**
