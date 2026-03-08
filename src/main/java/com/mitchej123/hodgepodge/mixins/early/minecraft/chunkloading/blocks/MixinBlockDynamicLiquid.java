ecea8e6edfd64d2e1fcfe016cbdac09c3a8cdb00
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-03-08 23:30:28 +0100

    schedule a block update when canceling

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/chunkloading/blocks/MixinBlockDynamicLiquid.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/chunkloading/blocks/MixinBlockDynamicLiquid.java
@@ -3,6 +3,8 @@ package com.mitchej123.hodgepodge.mixins.early.minecraft.chunkloading.blocks;
 import java.util.Random;
 
 import net.minecraft.block.BlockDynamicLiquid;
+import net.minecraft.block.BlockLiquid;
+import net.minecraft.block.material.Material;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,12 +13,17 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlockDynamicLiquid.class)
-public class MixinBlockDynamicLiquid {
+public abstract class MixinBlockDynamicLiquid extends BlockLiquid {
+
+    protected MixinBlockDynamicLiquid(Material p_i45413_1_) {
+        super(p_i45413_1_);
+    }
 
     @Inject(method = "updateTick", at = @At("HEAD"), cancellable = true)
     private void checkExist(World world, int x, int y, int z, Random rand, CallbackInfo ci) {
         final int offset = 6;
         if (!world.checkChunksExist(x - offset, y, z - offset, x + offset, y, z + offset)) {
+            world.scheduleBlockUpdate(x, y, z, this, this.tickRate(world) * 5);
             ci.cancel();
         }
     }
--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/chunkloading/blocks/MixinBlockFluidClassic.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/chunkloading/blocks/MixinBlockFluidClassic.java
@@ -2,8 +2,11 @@ package com.mitchej123.hodgepodge.mixins.early.minecraft.chunkloading.blocks;
 
 import java.util.Random;
 
+import net.minecraft.block.material.Material;
 import net.minecraft.world.World;
+import net.minecraftforge.fluids.BlockFluidBase;
 import net.minecraftforge.fluids.BlockFluidClassic;
+import net.minecraftforge.fluids.Fluid;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,12 +14,17 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlockFluidClassic.class)
-public class MixinBlockFluidClassic {
+public abstract class MixinBlockFluidClassic extends BlockFluidBase {
+
+    public MixinBlockFluidClassic(Fluid fluid, Material material) {
+        super(fluid, material);
+    }
 
     @Inject(method = "updateTick", at = @At("HEAD"), cancellable = true)
     private void checkExist(World world, int x, int y, int z, Random rand, CallbackInfo ci) {
         final int offset = 6;
         if (!world.checkChunksExist(x - offset, y, z - offset, x + offset, y, z + offset)) {
+            world.scheduleBlockUpdate(x, y, z, this, this.tickRate(world) * 5);
             ci.cancel();
         }
     }
