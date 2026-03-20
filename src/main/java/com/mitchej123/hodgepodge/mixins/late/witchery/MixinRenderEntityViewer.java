522951ff27d0da479dbb7839bf370f35efcac3ae
hinyb <40139991+hinyb@users.noreply.github.com>
2026-03-20 15:58:23 +0800

    style: apply spotless

--- src/main/java/com/mitchej123/hodgepodge/mixins/late/witchery/MixinRenderEntityViewer.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/witchery/MixinRenderEntityViewer.java
@@ -1,15 +1,16 @@
 package com.mitchej123.hodgepodge.mixins.late.witchery;
 
-import com.emoniph.witchery.client.RenderEntityViewer;
-import com.mitchej123.hodgepodge.compat.WitcheryCompatBridge;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.emoniph.witchery.client.RenderEntityViewer;
+import com.mitchej123.hodgepodge.compat.WitcheryCompatBridge;
+
 @Mixin(value = RenderEntityViewer.class, remap = false)
 public class MixinRenderEntityViewer {
+
     @Inject(method = "setOffset", at = @At(value = "TAIL"), remap = false)
     public void setOffset(float offset, CallbackInfo ci) {
         WitcheryCompatBridge.yOffset = offset;
