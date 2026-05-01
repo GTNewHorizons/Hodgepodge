72d0db79aa4f3e5db9e5a1aabc5dcd171b24cbe3
Charsy89 <28523197+Charsy89@users.noreply.github.com>
2026-05-01 01:01:37 -0600

    Updated Biomes O' Plenty Kelp mixin to use Redirect instead of ModifyArgs

--- src/main/java/com/mitchej123/hodgepodge/mixins/late/biomesoplenty/MixinWorldGenKelp.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/biomesoplenty/MixinWorldGenKelp.java
@@ -1,9 +1,12 @@
 package com.mitchej123.hodgepodge.mixins.late.biomesoplenty;
 
+import java.util.Random;
+
+import net.minecraft.world.World;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import biomesoplenty.common.world.features.WorldGenKelp;
 
@@ -14,17 +17,16 @@ public class MixinWorldGenKelp {
      * @author Charsy89
      * @reason Biomes O' Plenty causes cascading worldgen when generating kelp. This occurs because no +8 offset is
      *         added to the random coordinates provided to the generator by the game; this mixin modifies the X/Z
-     *         coordinates passed to generate() inside of setupGeneration() so that they are offset by +8 blocks.
+     *         coordinates passed to generate() so that they are offset by +8 blocks.
      */
-    @ModifyArgs(
+    @Redirect(
             method = "setupGeneration(Lnet/minecraft/world/World;Ljava/util/Random;Lbiomesoplenty/api/biome/BOPBiome;Ljava/lang/String;II)V",
             at = @At(
                     value = "INVOKE",
                     target = "Lbiomesoplenty/common/world/features/WorldGenKelp;generate(Lnet/minecraft/world/World;Ljava/util/Random;III)Z",
                     remap = true),
             remap = false)
-    private void hodgepodge$offsetKelpBy8(Args args) {
-        args.<Integer>set(2, args.<Integer>get(2) + 8); // randX
-        args.<Integer>set(4, args.<Integer>get(2) + 8); // randZ
+    private boolean hodgepodge$offsetKelpBy8(WorldGenKelp featureGen, World world, Random random, int x, int y, int z) {
+        return featureGen.generate(world, random, x + 8, y, z + 8);
     }
 }
