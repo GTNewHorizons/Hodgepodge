8256584e1c57ffad1ebd5410f3bf64a7abbe9065
GitHub GTNH Actions <>
2026-05-07 17:23:39 +0000

    spotlessApply

--- src/main/java/com/mitchej123/hodgepodge/mixins/late/ic2/leaks/MixinOverlayTesr.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/ic2/leaks/MixinOverlayTesr.java
@@ -1,13 +1,15 @@
 package com.mitchej123.hodgepodge.mixins.late.ic2.leaks;
 
-import ic2.core.block.OverlayTesr;
 import net.minecraft.client.renderer.RenderBlocks;
+
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import ic2.core.block.OverlayTesr;
 
 @Mixin(OverlayTesr.class)
 public class MixinOverlayTesr {
@@ -16,7 +18,9 @@ public class MixinOverlayTesr {
     @Shadow(remap = false)
     private RenderBlocks renderBlocks;
 
-    @Inject(method = "renderTileEntityAt", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/Tessellator;draw()I"))
+    @Inject(
+            method = "renderTileEntityAt",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/Tessellator;draw()I"))
     private void clearRef(CallbackInfo ci) {
         this.renderBlocks.blockAccess = null;
     }
