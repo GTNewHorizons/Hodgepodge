cc6bf489bff1ac687e5a4616be47d227af13db1f
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-05-07 15:54:06 +0200

    move allocation rate to hodgepodge existing f3 handler, add intcache to f3

--- src/main/java/com/mitchej123/hodgepodge/client/HodgepodgeClient.java
+++ src/main/java/com/mitchej123/hodgepodge/client/HodgepodgeClient.java
@@ -56,15 +56,12 @@ public class HodgepodgeClient {
 
         MinecraftForge.EVENT_BUS.register(new ReloadSoundsGui());
 
-        if (TweaksConfig.addSystemInfo) {
-            MinecraftForge.EVENT_BUS.register(DebugScreenHandler.INSTANCE);
-        }
+        MinecraftForge.EVENT_BUS.register(DebugScreenHandler.INSTANCE);
 
         if (DebugConfig.showChunkGenDebug || !FMLForgePlugin.RUNTIME_DEOBF) {
             MinecraftForge.EVENT_BUS.register(ChunkGenDebugHandler.INSTANCE);
         }
 
-        MinecraftForge.EVENT_BUS.register(new AllocationRateHUD(true));
         ClientCommandHandler.instance.registerCommand(new AllocationsCommand());
 
         FMLCommonHandler.instance().bus().register(new ClientKeyListener());
