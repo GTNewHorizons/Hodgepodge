0971329e3bf86bab9b0b8869024a46d1b99eddd2
Charsy89 <28523197+Charsy89@users.noreply.github.com>
2026-05-12 10:57:37 -0600

    Use ModifyVariable instead of Redirect

--- src/main/java/com/mitchej123/hodgepodge/mixins/late/biomesoplenty/MixinWorldGenKelp.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/biomesoplenty/MixinWorldGenKelp.java
@@ -1,12 +1,8 @@
 package com.mitchej123.hodgepodge.mixins.late.biomesoplenty;
 
-import java.util.Random;
-
-import net.minecraft.world.World;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import biomesoplenty.common.world.features.WorldGenKelp;
 
@@ -16,17 +12,32 @@ public class MixinWorldGenKelp {
     /**
      * @author Charsy89
      * @reason Biomes O' Plenty causes cascading worldgen when generating kelp. This occurs because no +8 offset is
-     *         added to the random coordinates provided to the generator by the game; this mixin modifies the X/Z
-     *         coordinates passed to generate() so that they are offset by +8 blocks.
+     *         added to the random coordinates provided to the generator by the game; this mixin modifies the X
+     *         coordinate passed to generate() so that it is offset by +8 blocks.
      */
-    @Redirect(
-            method = "setupGeneration(Lnet/minecraft/world/World;Ljava/util/Random;Lbiomesoplenty/api/biome/BOPBiome;Ljava/lang/String;II)V",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lbiomesoplenty/common/world/features/WorldGenKelp;generate(Lnet/minecraft/world/World;Ljava/util/Random;III)Z",
-                    remap = true),
-            remap = false)
-    private boolean hodgepodge$offsetKelpBy8(WorldGenKelp featureGen, World world, Random random, int x, int y, int z) {
-        return featureGen.generate(world, random, x + 8, y, z + 8);
+    @ModifyVariable(
+            method = "Lbiomesoplenty/common/world/features/WorldGenKelp;generate(Lnet/minecraft/world/World;Ljava/util/Random;III)Z",
+            argsOnly = true,
+            at = @At(value = "HEAD"),
+            remap = true,
+            ordinal = 0)
+    private int hodgepodge$offsetKelpXBy8(int x) {
+        return x + 8;
+    }
+
+    /**
+     * @author Charsy89
+     * @reason Biomes O' Plenty causes cascading worldgen when generating kelp. This occurs because no +8 offset is
+     *         added to the random coordinates provided to the generator by the game; this mixin modifies the X
+     *         coordinate passed to generate() so that it is offset by +8 blocks.
+     */
+    @ModifyVariable(
+            method = "Lbiomesoplenty/common/world/features/WorldGenKelp;generate(Lnet/minecraft/world/World;Ljava/util/Random;III)Z",
+            argsOnly = true,
+            at = @At(value = "HEAD"),
+            remap = true,
+            ordinal = 2)
+    private int hodgepodge$offsetKelpZBy8(int z) {
+        return z + 8;
     }
 }
