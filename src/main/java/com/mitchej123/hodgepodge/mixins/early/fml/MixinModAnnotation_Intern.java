077b42d548eae33ec0efc9cc368e65cc456d1daf
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-05-21 22:25:13 +0200

    intern values of ModAnnotation#addProperty

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/fml/MixinModAnnotation_Intern.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/fml/MixinModAnnotation_Intern.java
@@ -2,6 +2,7 @@ package com.mitchej123.hodgepodge.mixins.early.fml;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
@@ -30,5 +31,13 @@ public class MixinModAnnotation_Intern {
             at = @At("MIXINEXTRAS:EXPRESSION"))
     private String intern2(String original) {
         return ASMDataStringPooler.intern(original);
+    }
+
+    @ModifyVariable(method = "addProperty", at = @At("HEAD"), argsOnly = true, name = "arg2")
+    private Object intern2(Object original) {
+        if (original instanceof String s) {
+            return ASMDataStringPooler.intern(s);
+        }
+        return original;
     }
 }
