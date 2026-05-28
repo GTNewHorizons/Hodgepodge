4621890ab0d91c1c364176b2d3e6c2f634a09f28
koolkrafter5 <creepersrgreen@gmail.com>
2026-05-27 20:21:39 -0400

    Use an Overwrite to not need to allocate CallbackInfoReturnable<Boolean>

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinItem_EnchantGlint.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinItem_EnchantGlint.java
@@ -5,9 +5,7 @@ import net.minecraft.item.ItemPotion;
 import net.minecraft.item.ItemStack;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Overwrite;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -18,15 +16,14 @@ public abstract class MixinItem_EnchantGlint {
     /**
      * @author koolkrafter5
      * @reason Prevent enchantment glint from rendering once per render pass. Only render it on the final pass for all
-     *         items. Potions are special and have it on the first pass so only the liquid has the glint.
+     *         items. Rendering it earlier causes it to be behind later passes. Potions are special and have it on the
+     *         first pass so only the liquid has the glint (matches overwritten vanilla logic).
      */
-    @Inject(method = "hasEffect(Lnet/minecraft/item/ItemStack;I)Z", at = @At("HEAD"), cancellable = true, remap = false)
+    @Overwrite(remap = false)
     @SideOnly(Side.CLIENT)
-    private void fixGlint(ItemStack stack, int pass, CallbackInfoReturnable<Boolean> cir) {
-        Item item = (Item) (Object) this;
-        cir.setReturnValue(
-                stack != null && item.hasEffect(stack)
-                        && (item instanceof ItemPotion ? pass == 0
-                                : pass == item.getRenderPasses(stack.getItemDamage()) - 1));
+    public boolean hasEffect(ItemStack stack, int pass) {
+        final Item item = (Item) (Object) this;
+        return stack != null && item.hasEffect(stack)
+                && (item instanceof ItemPotion ? pass == 0 : pass == item.getRenderPasses(stack.getItemDamage()) - 1);
     }
 }
