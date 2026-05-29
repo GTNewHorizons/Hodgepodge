5649db1b0b6b68aaece46a219bde10c841130c5c
dipo33 <mato.dipcar@gmail.com>
2026-05-29 21:08:25 +0200

    Fix cursor item stack count hidden below potion effect panel

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinInventoryEffectRenderer_PotionEffectRendering.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinInventoryEffectRenderer_PotionEffectRendering.java
@@ -1,8 +1,10 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.InventoryEffectRenderer;
 import net.minecraft.inventory.Container;
+import net.minecraft.item.ItemStack;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -18,8 +20,8 @@ public abstract class MixinInventoryEffectRenderer_PotionEffectRendering extends
 
     /**
      * @author Alexdoru
-     * @reason Fix the bug that renders the potion effects above the tooltips from items in NEI Fix the vanilla bug that
-     *         doesn't render the potion effects that you get while your inventory is opened
+     * @reason Fix the bug that renders the potion effects above the tooltips from items in NEI. Fix the vanilla bug that
+     *         doesn't render the potion effects that you get while your inventory is opened.
      */
     @Overwrite
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
@@ -31,6 +33,13 @@ public abstract class MixinInventoryEffectRenderer_PotionEffectRendering extends
             this.func_147044_g();
         }
         if (leftPanelHidden) {
+            // renderItemOverlayIntoGUI skips depth writes, so the panel covers the count; re-render it after.
+            ItemStack cursorItem = this.mc.thePlayer.inventory.getItemStack();
+            if (cursorItem != null) {
+                FontRenderer font = cursorItem.getItem().getFontRenderer(cursorItem);
+                if (font == null) font = this.fontRendererObj;
+                itemRender.renderItemOverlayIntoGUI(font, this.mc.getTextureManager(), cursorItem, mouseX - 8, mouseY - 8, null);
+            }
             return;
         }
         super.drawScreen(mouseX, mouseY, partialTicks);
