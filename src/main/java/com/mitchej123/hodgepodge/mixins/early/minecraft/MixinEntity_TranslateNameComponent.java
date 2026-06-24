6e8bddae5621b7cfe80d6ea862da5e9953b96040
Nikolay S. <nicksitnikov@gmail.com>
2026-06-24 14:16:58 -0500

    Add localization to entity name chat components & give Wither Skeletons their own unique name (#931)

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinEntity_TranslateNameComponent.java
@@ -0,0 +1,35 @@
+package com.mitchej123.hodgepodge.mixins.early.minecraft;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.IChatComponent;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(Entity.class)
+public abstract class MixinEntity_TranslateNameComponent {
+
+    /**
+     * New implementation based on {@link Entity#getCommandSenderName()}. This may cause issues with mods that override
+     * {@link Entity#getCommandSenderName()} but not {@link Entity#func_145748_c_()}. (Specifically,
+     * {@code func_145748_c_} will return a translation key of {@code entity.[entity ID].name}, which may not be the
+     * same as the text returned by the mod's implementation of {@code getCommandSenderName}.)
+     *
+     * @author nicksitnikov
+     * @reason This method initially returned a component that was translated on the server. Replacing it with a
+     *         {@link ChatComponentTranslation} allows the name of the entity to be correctly translated to the client's
+     *         language.
+     */
+    @Overwrite
+    public IChatComponent func_145748_c_() {
+        String s = EntityList.getEntityString((Entity) (Object) this);
+
+        if (s == null) {
+            s = "generic";
+        }
+
+        return new ChatComponentTranslation("entity." + s + ".name");
+    }
+}
