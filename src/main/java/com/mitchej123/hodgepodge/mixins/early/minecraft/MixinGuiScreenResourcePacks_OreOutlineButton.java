6db92c016da0f71d10b8dd1d5fc75285c0a97094
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-08-11 22:35:43 +0200

    Button - add texture - move it a bit - 2 state - tooltip

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinGuiScreenResourcePacks_OreOutlineButton.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinGuiScreenResourcePacks_OreOutlineButton.java
@@ -1,10 +1,17 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
+import java.util.Collections;
+
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiScreenResourcePacks;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.util.ResourceLocation;
 
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,10 +19,44 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GuiScreenResourcePacks.class)
 public class MixinGuiScreenResourcePacks_OreOutlineButton extends GuiScreen {
 
+    @Unique
+    private static final ResourceLocation hodgepodge$icon = new ResourceLocation(
+            "hodgepodge",
+            "textures/gui/outline_button.png");
+
+    @Unique
+    private static final int hodgepodge$buttonId = 9001;
+
+    @Unique
+    private boolean hodgepodge$outlineEnabled = false;
+
     @Inject(method = "initGui", at = @At("TAIL"))
     private void hodgepodge$addOreOutlineButton(CallbackInfo ci) {
-        int buttonX = this.width / 2 - 182;
+        this.buttonList.add(new GuiButton(hodgepodge$buttonId, this.width / 2 - 204, this.height - 48, 20, 20, ""));
+    }
+
+    @Inject(method = "actionPerformed", at = @At("HEAD"))
+    private void hodgepodge$onActionPerformed(GuiButton button, CallbackInfo ci) {
+        if (button.id == hodgepodge$buttonId) {
+            hodgepodge$outlineEnabled = !hodgepodge$outlineEnabled;
+        }
+    }
+
+    @Inject(method = "drawScreen", at = @At("TAIL"))
+    private void hodgepodge$drawOreOutlineIcon(int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
+        int iconX = this.width / 2 - 202;
+        int iconY = this.height - 46;
+
+        this.mc.getTextureManager().bindTexture(hodgepodge$icon);
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        Gui.func_146110_a(iconX, iconY, 0, hodgepodge$outlineEnabled ? 16 : 0, 16, 16, 16, 32);
+
+        int buttonX = this.width / 2 - 204;
         int buttonY = this.height - 48;
-        this.buttonList.add(new GuiButton(9001, buttonX, buttonY, 20, 20, ""));
+        if (mouseX >= buttonX && mouseX < buttonX + 20 && mouseY >= buttonY && mouseY < buttonY + 20) {
+            String key = hodgepodge$outlineEnabled ? "hodgepodge.resourcepacks.ore_outline.enabled"
+                    : "hodgepodge.resourcepacks.ore_outline.disabled";
+            this.drawHoveringText(Collections.singletonList(I18n.format(key)), mouseX, mouseY, this.fontRendererObj);
+        }
     }
 }
