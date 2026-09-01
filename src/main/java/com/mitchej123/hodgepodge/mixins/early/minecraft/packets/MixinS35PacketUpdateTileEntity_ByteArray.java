f5ea08fb26f350ce4bbfbc25b1839fad50ef4108
GitHub GTNH Actions <>
2026-09-01 09:00:00 +0000

    spotlessApply

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/packets/MixinS35PacketUpdateTileEntity_ByteArray.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/packets/MixinS35PacketUpdateTileEntity_ByteArray.java
@@ -14,8 +14,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
- * Optimize NBT of the form {[one character]: [byte array]}, e.g. {"X": [B;0b, 1b]}. This skips the compression and the NBT
- * overhead on the network. NBT that does not follow this will get sent normally with an extra byte instead.
+ * Optimize NBT of the form {[one character]: [byte array]}, e.g. {"X": [B;0b, 1b]}. This skips the compression and the
+ * NBT overhead on the network. NBT that does not follow this will get sent normally with an extra byte instead.
  */
 @Mixin(S35PacketUpdateTileEntity.class)
 public class MixinS35PacketUpdateTileEntity_ByteArray {
