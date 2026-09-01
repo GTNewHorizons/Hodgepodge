5f6012a7e78e31fb02ace1294a401d7b573d2086
playfuldoggo <chloef31@outlook.com>
2026-09-01 07:01:30 +0100

    Removed unnecessary imports

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinTileEntityFurnace_FixFuelOverflow.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinTileEntityFurnace_FixFuelOverflow.java
@@ -1,11 +1,9 @@
 package com.mitchej123.hodgepodge.mixins.early.minecraft;
 
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntityFurnace;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
