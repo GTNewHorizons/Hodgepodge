c4ca81556a9b9ee2ae21f5c096772997db4063fc
nicksitnikov <nicksitnikov@gmail.com>
2026-09-13 01:01:40 -0500

    Fix interaction with `decrStackSize`

--- src/main/java/com/mitchej123/hodgepodge/mixins/late/bibliocraft/MixinTileEntityArmorStand_MarkDirty.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/late/bibliocraft/MixinTileEntityArmorStand_MarkDirty.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import jds.bibliocraft.tileentities.TileEntityArmorStand;
 
@@ -15,6 +16,11 @@ public class MixinTileEntityArmorStand_MarkDirty extends TileEntity {
 
     @Inject(method = "setInventorySlotContents", at = @At("TAIL"))
     private void triggerMarkDirty(int slot, ItemStack stack, CallbackInfo ci) {
+        this.markDirty();
+    }
+
+    @Inject(method = "decrStackSize", at = @At("TAIL"))
+    private void triggerMarkDirty2(int slot, int amount, CallbackInfoReturnable<ItemStack> cir) {
         this.markDirty();
     }
 }
