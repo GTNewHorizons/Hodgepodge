c2554156d7a1a4cddbec074b80a055f6e3d784ba
playfuldoggo <chloef31@outlook.com>
2026-09-15 04:22:24 +0100

    quickly ran spotlessApply

--- src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinTileEntityFurnace_FixFuelOverflow.java
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinTileEntityFurnace_FixFuelOverflow.java
@@ -8,8 +8,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import com.llamalad7.mixinextras.sugar.Local;
-
 @Mixin(TileEntityFurnace.class)
 public class MixinTileEntityFurnace_FixFuelOverflow {
 
