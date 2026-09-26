7972d3089f9c6993f8de131626368943924ee250
Eclipse-Sol <phillips-l@outlook.com>
2026-09-26 16:34:44 -0400

    Fix Flatworld Customization NPE (#956)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/main/java/com/mitchej123/hodgepodge/mixins/early/minecraft/MixinGuiCreateFlatWorld_FixItemCrash.java
@@ -0,0 +1,35 @@
+package com.mitchej123.hodgepodge.mixins.early.minecraft;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.gen.FlatLayerInfo;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import com.llamalad7.mixinextras.sugar.Local;
+
+@Mixin(targets = "net.minecraft.client.gui.GuiCreateFlatWorld$Details")
+public class MixinGuiCreateFlatWorld_FixItemCrash {
+
+    /**
+     * Setting a layer with no registered ItemBlock produces an NPE. Need to bypass and set name.
+     */
+    @ModifyVariable(method = "drawSlot", at = @At(value = "STORE"), name = "itemstack")
+    private ItemStack hodgepodge$nullIfMissingItemBlock(ItemStack itemstack) {
+        return itemstack != null && itemstack.getItem() == null ? null : itemstack;
+    }
+
+    @ModifyVariable(method = "drawSlot", at = @At(value = "STORE"), name = "s")
+    private String hodgepodge$fixMissingItemBlockLabel(String s,
+            @Local(name = "flatlayerinfo") FlatLayerInfo flatlayerinfo) {
+        Block block = flatlayerinfo.func_151536_b();
+        if (block == Blocks.air || Item.getItemFromBlock(block) != null) {
+            return s;
+        }
+        return block.getLocalizedName();
+    }
+}
